57ae73aef7d419ffd701ccd9d446bfb7fcc30c4a
dennis_lloyd_jr <dennis_lloyd_jr@91d956da-2944-4949-a11c-b487c32f045b>
2010-09-01 21:03:21 +0000

    Added ability to execute a command in a new connection from within a test case.

---
+++ tSQLtCLR/tSQLtCLR/CommandExecutor.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlTypes;
+using Microsoft.SqlServer.Server;
+using System.Data.SqlClient;
+using System.Transactions;
+using System.Security;
+
+namespace tSQLtCLR
+{
+    class CommandExecutor
+    {
+        public void Execute(SqlString command)
+        {
+
+            SqlConnection conn = null;
+            try
+            {
+
+                using (TransactionScope scope = new TransactionScope(TransactionScopeOption.Suppress))
+                {
+                    String connectionString = CreateConnectionStringToContextDatabase();
+                    conn = new SqlConnection(connectionString);
+
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = conn;
+                    cmd.CommandText = command.ToString();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (SecurityException se)
+            {
+                throw new CommandExecutorException("Error connecting to database. You may need to create tSQLt assembly with EXTERNAL_ACCESS.", se);
+            }
+            finally
+            {
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+        private static String CreateConnectionStringToContextDatabase()
+        {
+            TestDatabaseFacade facade = new TestDatabaseFacade();
+            String server = facade.ServerName;
+            String database = facade.DatabaseName;
+            System.Data.SqlClient.SqlConnectionStringBuilder builder =
+              new System.Data.SqlClient.SqlConnectionStringBuilder();
+            builder["Data Source"] = server;
+            builder["Integrated Security"] = true;
+            builder["Initial Catalog"] = database;
+            String connectionString = builder.ConnectionString;
+            return connectionString;
+        }
+    }
+}
---
+++ tSQLtCLR/tSQLtCLR/CommandExecutorException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace tSQLtCLR
+{
+    [Serializable]
+    public class CommandExecutorException : Exception
+    {
+        public CommandExecutorException() : base() { }
+        public CommandExecutorException(String message) : base(message) { }
+        public CommandExecutorException(String message, Exception innerException) : base(message, innerException) { }
+        protected CommandExecutorException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
