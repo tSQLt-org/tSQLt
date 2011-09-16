e988df7a0a5808ea9b150d0fb71e5216fa39f000
maibotian <maibotian@91d956da-2944-4949-a11c-b487c32f045b>
2011-09-16 03:00:16 +0000

    added test for special chars in xml result -> no code change

--- tSQLt.class.sql
+++ tSQLt.class.sql
@@ -125,7 +125,7 @@ CREATE PROCEDURE tSQLt.Private_PrintXML
     @Message XML
 AS 
 BEGIN
-    SELECT @Message FOR XML PATH('');
+    SELECT @Message FOR XML PATH('');--Required together with ":XML ON" sqlcmd statement to allow more than 1mb to be returned
     RETURN 0;
 END;
 GO
