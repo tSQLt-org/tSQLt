4382556a1b9947d6baf147a53bee99e512e5c709
dennis_lloyd_jr <dennis_lloyd_jr@91d956da-2944-4949-a11c-b487c32f045b>
2011-11-21 11:45:19 +0000

    Made tSQLt.Private_TestClasses public as tSQLt.TestClasses.

--- Tests/tSQLt_test.class.sql
+++ Tests/tSQLt_test.class.sql
@@ -4053,5 +4053,85 @@ BEGIN
 END;
 GO
 
+
+CREATE PROCEDURE tSQLt_test.[test tSQLt.TestClasses returns no test classes when there are no test classes]
+AS
+BEGIN
+  EXEC tSQLt_testutil.RemoveTestClassPropertyFromAllExistingClasses;
+
+  SELECT *
+    INTO #Actual
+    FROM tSQLt.TestClasses;
+    
+  SELECT TOP(0) * 
+    INTO #Expected
+    FROM #Actual;
+    
+  EXEC tSQLt.AssertEqualsTable '#Expected', '#Actual';
+END;
+GO
+
+
+CREATE PROCEDURE tSQLt_test.[test tSQLt.TestClasses returns single test class]
+AS
+BEGIN
+  EXEC tSQLt_testutil.RemoveTestClassPropertyFromAllExistingClasses;
+  EXEC tSQLt.NewTestClass 'tSQLt_test_dummy_A';
+
+  SELECT Name
+    INTO #Actual
+    FROM tSQLt.TestClasses;
+    
+  SELECT TOP(0) * 
+    INTO #Expected
+    FROM #Actual;
+  
+  INSERT INTO #Expected(Name) VALUES ('tSQLt_test_dummy_A');
+    
+  EXEC tSQLt.AssertEqualsTable '#Expected', '#Actual';
+END;
+GO
+
+CREATE PROCEDURE tSQLt_test.[test tSQLt.TestClasses returns multiple test classes]
+AS
+BEGIN
+  EXEC tSQLt_testutil.RemoveTestClassPropertyFromAllExistingClasses;
+  EXEC tSQLt.NewTestClass 'tSQLt_test_dummy_A';
+  EXEC tSQLt.NewTestClass 'tSQLt_test_dummy_B';
+
+  SELECT Name
+    INTO #Actual
+    FROM tSQLt.TestClasses;
+    
+  SELECT TOP(0) * 
+    INTO #Expected
+    FROM #Actual;
+  
+  INSERT INTO #Expected(Name) VALUES ('tSQLt_test_dummy_A');
+  INSERT INTO #Expected(Name) VALUES ('tSQLt_test_dummy_B');
+    
+  EXEC tSQLt.AssertEqualsTable '#Expected', '#Actual';
+END;
+GO
+
+CREATE PROCEDURE tSQLt_test.[test tSQLt.TestClasses returns other important columns]
+AS
+BEGIN
+  EXEC tSQLt_testutil.RemoveTestClassPropertyFromAllExistingClasses;
+  EXEC tSQLt.NewTestClass 'tSQLt_test_dummy_A';
+
+  SELECT Name,SchemaId
+    INTO #Actual
+    FROM tSQLt.TestClasses;
+    
+  SELECT TOP(0) * 
+    INTO #Expected
+    FROM #Actual;
+  
+  INSERT INTO #Expected(Name, SchemaId) VALUES ('tSQLt_test_dummy_A',SCHEMA_ID('tSQLt_test_dummy_A'));
+    
+  EXEC tSQLt.AssertEqualsTable '#Expected', '#Actual';
+END;
+GO
 --ROLLBACK
 --tSQLt_test
