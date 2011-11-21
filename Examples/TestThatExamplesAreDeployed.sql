2f79a0452326539f32626ed1146179cf6a6f5540
dennis_lloyd_jr <dennislloydjr@gmail.com>
2011-11-21 15:36:23 +0000

    Updated build to make sure that example database is packaged and deployed.

---
+++ Examples/TestThatExamplesAreDeployed.sql
@@ -0,0 +1,32 @@
+EXEC tSQLt.NewTestClass 'ExampleDeployed';
+GO
+
+CREATE PROCEDURE ExampleDeployed.[test example tests have appropriate successes and failures]
+AS
+BEGIN
+  BEGIN TRY
+    EXEC tSQLt.RunTestClass 'AcceleratorTests';
+  END TRY
+  BEGIN CATCH
+  END CATCH
+  
+  SELECT TestCase, Result
+    INTO #Actual
+    FROM tSQLt.TestResult;
+  
+  SELECT TOP(0) *
+    INTO #Expected
+    FROM #Actual;
+
+  INSERT INTO #Expected (TestCase, Result) 
+       VALUES ('test no particles are in a rectangle when there are no particles in the table', 'Success');
+  INSERT INTO #Expected (TestCase, Result)
+       VALUES ('test a particle within the rectangle is returned', 'Success');
+  INSERT INTO #Expected (TestCase, Result)
+       VALUES ('test a particle within the rectangle is returned with an Id, Point Location and Value', 'Success');
+  INSERT INTO #Expected (TestCase, Result)
+       VALUES ('test a particle is included only if it fits inside the boundaries of the rectangle', 'Failure');
+  
+  EXEC tSQLt.AssertEqualsTable '#Expected', '#Actual';
+END;
+GO
