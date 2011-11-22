80c412430f5dfce5787aa64f498ec7cbcc7b34fb
maibotian <maibotian@91d956da-2944-4949-a11c-b487c32f045b>
2011-11-22 11:26:52 +0000

    Added comments to test case examples. Added SpyProcedure examples.

--- Examples/Source.sql
+++ Examples/Source.sql
@@ -16,3 +16,28 @@ AS RETURN (
          Y > @Y1 AND Y <= @Y2        -- "<= @Y2" instead of "< @Y2" to show missing 'actual' row
 );
 GO
+
+IF OBJECT_ID('Accelerator.SendHiggsBosonDiscoveryEmail') IS NOT NULL DROP PROCEDURE Accelerator.SendHiggsBosonDiscoveryEmail;
+GO
+
+CREATE PROCEDURE Accelerator.SendHiggsBosonDiscoveryEmail
+  @EmailAddress NVARCHAR(MAX)
+AS
+BEGIN
+  RAISERROR('Not Implemented - yet',16,10);
+END;
+GO
+
+IF OBJECT_ID('Accelerator.AlertParticleDiscovered') IS NOT NULL DROP PROCEDURE Accelerator.AlertParticleDiscovered;
+GO
+
+CREATE PROCEDURE Accelerator.AlertParticleDiscovered
+  @ParticleDiscovered NVARCHAR(MAX)
+AS
+BEGIN
+  IF @ParticleDiscovered = 'Higgs Boson'
+  BEGIN
+    EXEC Accelerator.SendHiggsBosonDiscoveryEmail 'particle-discovery@new-era-particles.tsqlt.org';
+  END;
+END;
+GO
--- Examples/Tests.sql
+++ Examples/Tests.sql
@@ -4,13 +4,16 @@ GO
 CREATE PROCEDURE AcceleratorTests.[test no particles are in a rectangle when there are no particles in the table]
 AS
 BEGIN
+  --Assemble: Fake the Particle table to make sure it is empty
   EXEC tSQLt.FakeTable 'Accelerator.Particle';
 
   DECLARE @ParticlesInRectangle INT;
   
+  --Act: Call the  GetParticlesInRectangle Table-Valued Function and capture the number of rows it returns.
   SELECT @ParticlesInRectangle = COUNT(1)
     FROM Accelerator.GetParticlesInRectangle(0.0, 0.0, 1.0, 1.0);
   
+  --Assert: Check that 0 rows were returned
   EXEC tSQLt.AssertEquals 0, @ParticlesInRectangle;
 END;
 GO
@@ -18,19 +21,25 @@ GO
 CREATE PROCEDURE AcceleratorTests.[test a particle within the rectangle is returned]
 AS
 BEGIN
+  --Assemble: Fake the Particle table to make sure it is empty and that constraints will not be a problem
   EXEC tSQLt.FakeTable 'Accelerator.Particle';
+  --          Put a test particle into the table
   INSERT INTO Accelerator.Particle (Id, X, Y) VALUES (1, 0.5, 0.5);
   
+  --Act: Call the  GetParticlesInRectangle Table-Valued Function and capture the Id column into the #Actual temp table
   SELECT Id
     INTO #Actual
     FROM Accelerator.GetParticlesInRectangle(0.0, 0.0, 1.0, 1.0);
-    
+  
+  --Assert: Create an empty #Expected temp table that has the same structure as the #Actual table
   SELECT TOP(0) *
     INTO #Expected
     FROM #Actual;
-    
+  
+  --        A single row with an Id value of 1 is expected
   INSERT INTO #Expected (Id) VALUES (1);
 
+  --        Compare the data in the #Expected and #Actual tables
   EXEC tSQLt.AssertEqualsTable '#Expected', '#Actual';
 END;
 GO
@@ -38,19 +47,25 @@ GO
 CREATE PROCEDURE AcceleratorTests.[test a particle within the rectangle is returned with an Id, Point Location and Value]
 AS
 BEGIN
+  --Assemble: Fake the Particle table to make sure it is empty and that constraints will not be a problem
   EXEC tSQLt.FakeTable 'Accelerator.Particle';
+  --          Put a test particle into the table
   INSERT INTO Accelerator.Particle (Id, X, Y, Value) VALUES (1, 0.5, 0.5, 'MyValue');
   
+  --Act: Call the  GetParticlesInRectangle Table-Valued Function and capture the relevant columns into the #Actual temp table
   SELECT Id, X, Y, Value
     INTO #Actual
     FROM Accelerator.GetParticlesInRectangle(0.0, 0.0, 1.0, 1.0);
     
+  --Assert: Create an empty #Expected temp table that has the same structure as the #Actual table
   SELECT TOP(0) *
     INTO #Expected
     FROM #Actual;
     
+  --        A single row with the expected data is inserted into the #Expected table
   INSERT INTO #Expected (Id, X, Y, Value) VALUES (1, 0.5, 0.5, 'MyValue');
 
+  --        Compare the data in the #Expected and #Actual tables
   EXEC tSQLt.AssertEqualsTable '#Expected', '#Actual';
 END;
 GO
@@ -58,7 +73,9 @@ GO
 CREATE PROCEDURE AcceleratorTests.[test a particle is included only if it fits inside the boundaries of the rectangle]
 AS
 BEGIN
+  --Assemble: Fake the Particle table to make sure it is empty and that constraints will not be a problem
   EXEC tSQLt.FakeTable 'Accelerator.Particle';
+  --          Populate the Particle table with rows that hug the rectangle boundaries
   INSERT INTO Accelerator.Particle (Id, X, Y) VALUES ( 1, -0.01,  0.50);
   INSERT INTO Accelerator.Particle (Id, X, Y) VALUES ( 2,  0.00,  0.50);
   INSERT INTO Accelerator.Particle (Id, X, Y) VALUES ( 3,  0.01,  0.50);
@@ -72,19 +89,79 @@ BEGIN
   INSERT INTO Accelerator.Particle (Id, X, Y) VALUES (11,  0.50,  1.00);
   INSERT INTO Accelerator.Particle (Id, X, Y) VALUES (12,  0.50,  1.01);
   
+  --Act: Call the  GetParticlesInRectangle Table-Valued Function and capture the relevant columns into the #Actual temp table
   SELECT Id, X, Y
     INTO #Actual
     FROM Accelerator.GetParticlesInRectangle(0.0, 0.0, 1.0, 1.0);
     
+  --Assert: Create an empty #Expected temp table that has the same structure as the #Actual table
   SELECT TOP(0) *
     INTO #Expected
     FROM #Actual;
     
+  --        The expected data is inserted into the #Expected table
   INSERT INTO #Expected (Id, X, Y) VALUES (3,  0.01, 0.50);
   INSERT INTO #Expected (Id, X, Y) VALUES (4,  0.99, 0.50);
   INSERT INTO #Expected (Id, X, Y) VALUES (9,  0.50, 0.01);
   INSERT INTO #Expected (Id, X, Y) VALUES (10, 0.50, 0.99);
     
+  --        Compare the data in the #Expected and #Actual tables
   EXEC tSQLt.AssertEqualsTable '#Expected', '#Actual';
+END;
+GO
+
+CREATE PROCEDURE AcceleratorTests.[test email is sent if we detected a higgs-boson]
+AS
+BEGIN
+  --Assemble: Replace the SendHiggsBosonDiscoveryEmail with a spy. 
+  EXEC tSQLt.SpyProcedure 'Accelerator.SendHiggsBosonDiscoveryEmail';
+  
+  --Act: Call the AlertParticleDiscovered procedure - this is the procedure being tested.
+  EXEC Accelerator.AlertParticleDiscovered 'Higgs Boson';
+  
+  --Assert: A spy records the parameters passed to the procedure in a *_SpyProcedureLog table. 
+  --        Copy the EmailAddress parameter values that the spy recorded into the #Actual temp table.
+  SELECT EmailAddress
+    INTO #Actual
+    FROM Accelerator.SendHiggsBosonDiscoveryEmail_SpyProcedureLog;
+    
+  --        Create an empty #Expected temp table that has the same structure as the #Actual table
+  SELECT TOP(0) * INTO #Expected FROM #Actual;
+  
+  --        Add a row to the #Expected table with the expected email address.
+  INSERT INTO #Expected 
+    (EmailAddress)
+  VALUES 
+    ('particle-discovery@new-era-particles.tsqlt.org');
+
+  --        Compare the data in the #Expected and #Actual tables
+  EXEC tSQLt.AssertEqualsTable '#Expected', '#Actual';
+END;
+GO
+
+
+CREATE PROCEDURE AcceleratorTests.[test email is not sent if we detected something other than higgs-boson]
+AS
+BEGIN
+  --Assemble: Replace the SendHiggsBosonDiscoveryEmail with a spy. 
+  EXEC tSQLt.SpyProcedure 'Accelerator.SendHiggsBosonDiscoveryEmail';
+  
+  --Act: Call the AlertParticleDiscovered procedure - this is the procedure being tested.
+  EXEC Accelerator.AlertParticleDiscovered 'Proton';
+  
+  --Assert: A spy records the parameters passed to the procedure in a *_SpyProcedureLog table. 
+  --        Copy the EmailAddress parameter values that the spy recorded into the #Actual temp table.
+  SELECT EmailAddress
+    INTO #Actual
+    FROM Accelerator.SendHiggsBosonDiscoveryEmail_SpyProcedureLog;
+    
+  --        Create an empty #Expected temp table that has the same structure as the #Actual table
+  SELECT TOP(0) * INTO #Expected FROM #Actual;
+  
+  --        The SendHiggsBosonDiscoveryEmail should not have been called. So the #Expected table is empty.
+  
+  --        Compare the data in the #Expected and #Actual tables
+  EXEC tSQLt.AssertEqualsTable '#Expected', '#Actual';
+
 END;
 GO
