6f58ffb8d3068ef3bcec7138f45f4581fd204354
dennis_lloyd_jr <dennislloydjr@gmail.com>
2011-11-23 19:59:48 +0000

    Added ApplyConstraint examples to the example script.

--- Examples/CreateDatabase.sql
+++ Examples/CreateDatabase.sql
@@ -19,12 +19,21 @@ GO
 CREATE SCHEMA Accelerator;
 GO
 
-IF OBJECT_ID('Accelerator.Particle') IS NOT NULL DROP TABLE Practice.Particle;
+IF OBJECT_ID('Accelerator.Particle') IS NOT NULL DROP TABLE Accelerator.Particle;
 GO
 CREATE TABLE Accelerator.Particle(
   Id INT IDENTITY(1,1) NOT NULL CONSTRAINT PK_Point_Id PRIMARY KEY,
   X DECIMAL(10,2) NOT NULL,
   Y DECIMAL(10,2) NOT NULL,
-  Value NVARCHAR(MAX) NOT NULL
+  Value NVARCHAR(MAX) NOT NULL,
+  ColorId INT NOT NULL
+);
+GO
+
+IF OBJECT_ID('Accelerator.Color') IS NOT NULL DROP TABLE Practice.Color;
+GO
+CREATE TABLE Accelerator.Color(
+  Id INT IDENTITY(1,1) NOT NULL CONSTRAINT PK_Color_Id PRIMARY KEY,
+  ColorName NVARCHAR(MAX) NOT NULL
 );
 GO
--- Examples/Tests.sql
+++ Examples/Tests.sql
@@ -223,3 +223,57 @@ BEGIN
   EXEC tSQLt.AssertEqualsString 'The Accelerator is prepared with 3 particles.', @StatusMessage;
 END;
 GO
+
+CREATE PROCEDURE AcceleratorTests.[test foreign key violated if Particle color is not in Color table]
+AS
+BEGIN
+  --Assemble: Fake the Particle and the Color tables to make sure they are empty and other 
+  --          constraints will not be a problem
+  EXEC tSQLt.FakeTable 'Accelerator.Particle';
+  EXEC tSQLt.FakeTable 'Accelerator.Color';
+  --          Put the FK_ParticleColor foreign key constraint back onto the Particle table
+  --          so we can test it.
+  EXEC tSQLt.ApplyConstraint 'Accelerator.Particle', 'FK_ParticleColor';
+  
+  --Act: Attempt to insert a record into the Particle table without any records in Color table.
+  --     We expect an exception to happen, so we capture the ERROR_MESSAGE()
+  DECLARE @err NVARCHAR(MAX); SET @err = '<No Exception Thrown!>';
+  BEGIN TRY
+    INSERT INTO Accelerator.Particle (ColorId) VALUES (7);
+  END TRY
+  BEGIN CATCH
+    SET @err = ERROR_MESSAGE();
+  END CATCH
+  
+  --Assert: Check that trying to insert the record resulted in the FK_ParticleColor foreign key being violated.
+  --        If no exception happened the value of @err is still '<No Exception Thrown>'.
+  IF (@err NOT LIKE '%FK_ParticleColor%')
+  BEGIN
+    EXEC tSQLt.Fail 'Expected exception (FK_ParticleColor exception) not thrown. Instead:',@err;
+  END;
+END;
+GO
+
+CREATE PROC AcceleratorTests.[test foreign key is not violated if Particle color is in Color table]
+AS
+BEGIN
+  --Assemble: Fake the Particle and the Color tables to make sure they are empty and other 
+  --          constraints will not be a problem
+  EXEC tSQLt.FakeTable 'Accelerator.Particle';
+  EXEC tSQLt.FakeTable 'Accelerator.Color';
+  --          Put the FK_ParticleColor foreign key constraint back onto the Particle table
+  --          so we can test it.
+  EXEC tSQLt.ApplyConstraint 'Accelerator.Particle', 'FK_ParticleColor';
+  
+  --          Insert a record into the Color table. We'll reference this Id again in the Act
+  --          step.
+  INSERT INTO Accelerator.Color (Id) VALUES (7);
+  
+  --Act: Attempt to insert a record into the Particle table.
+  INSERT INTO Accelerator.Particle (ColorId) VALUES (7);
+  
+  --Assert: If any exception was thrown, the test will automatically fail. Therefore, the test
+  --        passes as long as there was no exception. This is one of the VERY rare cases when
+  --        at test case does not have an Assert step.
+END
+GO
