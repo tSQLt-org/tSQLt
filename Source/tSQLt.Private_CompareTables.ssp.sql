fae5e33f421c566cd56b60cf1366282a57150cbf
dennis_lloyd_jr <dennislloydjr@gmail.com>
2012-05-29 22:10:11 +0000

    More progress on AssertEqualsTable refactoring

---
+++ Source/tSQLt.Private_CompareTables.ssp.sql
@@ -0,0 +1,60 @@
+IF OBJECT_ID('tSQLt.Private_CompareTables') IS NOT NULL DROP PROCEDURE tSQLt.Private_CompareTables;
+GO
+---BUILD+
+CREATE PROCEDURE tSQLt.Private_CompareTables
+    @Expected NVARCHAR(MAX),
+    @Actual NVARCHAR(MAX),
+    @ResultTable NVARCHAR(MAX),
+    @ColumnList NVARCHAR(MAX),
+    @MatchIndicatorColumnName NVARCHAR(MAX)
+AS
+BEGIN
+    DECLARE @cmd NVARCHAR(MAX);
+    DECLARE @RestoredRowIndexCounterColName NVARCHAR(MAX);
+    SET @RestoredRowIndexCounterColName = @MatchIndicatorColumnName + '_RR';
+    
+    SELECT @cmd = 
+    '
+    INSERT INTO ' + @ResultTable + '
+    SELECT 
+      CASE 
+        WHEN RestoredRowIndex.'+@RestoredRowIndexCounterColName+' <= MinMax.Mn
+         THEN ''='' 
+        WHEN RestoredRowIndex.'+@RestoredRowIndexCounterColName+' <= [_{Left}_] 
+         THEN ''<'' 
+        ELSE ''>'' 
+      END AS ' + @MatchIndicatorColumnName + ', ' + @ColumnList + '
+    FROM(
+      SELECT MAX([_{Left}_]) AS [_{Left}_], 
+             MAX([_{Right}_]) AS [_{Right}_], 
+             ' + @ColumnList + ' 
+      FROM (
+        SELECT COUNT(1) AS [_{Left}_], 0[_{Right}_], ' + @ColumnList + '
+          FROM ' + @Expected + '
+         GROUP BY ' + @ColumnList + ' 
+        UNION ALL 
+        SELECT 0[_{Left}_], COUNT(1) AS [_{Right}_], ' + @ColumnList + ' 
+          FROM ' + @Actual + '
+         GROUP BY ' + @ColumnList + ' 
+      ) AS X 
+      GROUP BY ' + @ColumnList + ' 
+    ) AS CollapsedRows
+    CROSS APPLY (SELECT MIN(x), MAX(x) 
+                   FROM(
+                     SELECT [_{Left}_] x 
+                     UNION ALL 
+                     SELECT [_{Right}_]
+                     ) AS X
+                 ) AS MinMax(Mn,Mx)
+    CROSS APPLY (SELECT * FROM tSQLt.F_Num(MinMax.Mx)) AS RestoredRowIndex('+@RestoredRowIndexCounterColName+');';
+    
+    EXEC (@cmd);
+    
+    
+    SET @cmd = 'SET @r = CASE WHEN EXISTS(SELECT 1 FROM ' + @ResultTable + ' WHERE ' + @MatchIndicatorColumnName + ' IN (''<'', ''>'')) THEN 1 ELSE 0 END';
+    DECLARE @UnequalRowsExist INT;
+    EXEC sp_executesql @cmd, N'@r INT OUTPUT',@UnequalRowsExist OUTPUT;
+    
+    RETURN @UnequalRowsExist;
+END;
+---Build-
--- Tests/AssertEqualsTableTests.class.sql
+++ Tests/AssertEqualsTableTests.class.sql
@@ -175,6 +175,128 @@ BEGIN
 END;
 GO
 
+CREATE PROCEDURE AssertEqualsTableTests.[test compare table with two columns and no rows]
+AS
+BEGIN
+   CREATE TABLE AssertEqualsTableTests.LeftTable (a INT, b INT);
+
+   CREATE TABLE AssertEqualsTableTests.RightTable (a INT, b INT);
+   
+   CREATE TABLE AssertEqualsTableTests.ResultTable ([_m_] CHAR(1), a INT, b INT);
+   EXEC tSQLt.AssertEqualsTable 'AssertEqualsTableTests.LeftTable', 'AssertEqualsTableTests.RightTable';
+END;
+GO
+
+CREATE PROCEDURE AssertEqualsTableTests.[test same single row in each table with two columns]
+AS
+BEGIN
+   CREATE TABLE AssertEqualsTableTests.LeftTable (a INT, b INT);
+   INSERT INTO AssertEqualsTableTests.LeftTable VALUES (1, 2);
+
+   CREATE TABLE AssertEqualsTableTests.RightTable (a INT, b INT);
+   INSERT INTO AssertEqualsTableTests.RightTable VALUES (1, 2);
+   
+   EXEC tSQLt.AssertEqualsTable 'AssertEqualsTableTests.LeftTable', 'AssertEqualsTableTests.RightTable';
+   
+END;
+GO
+
+CREATE PROCEDURE AssertEqualsTableTests.[test same multiple rows in each table with two columns]
+AS
+BEGIN
+   CREATE TABLE AssertEqualsTableTests.LeftTable (a INT, b INT);
+   INSERT INTO AssertEqualsTableTests.LeftTable VALUES (1, 2);
+   INSERT INTO AssertEqualsTableTests.LeftTable VALUES (3, 4);
+
+   CREATE TABLE AssertEqualsTableTests.RightTable (a INT, b INT);
+   INSERT INTO AssertEqualsTableTests.RightTable VALUES (1, 2);
+   INSERT INTO AssertEqualsTableTests.RightTable VALUES (3, 4);
+   
+   EXEC tSQLt.AssertEqualsTable 'AssertEqualsTableTests.LeftTable', 'AssertEqualsTableTests.RightTable';
+   
+END;
+GO
+
+CREATE PROCEDURE AssertEqualsTableTests.[test multiple rows with one mismatching row with two columns]
+AS
+BEGIN
+   CREATE TABLE AssertEqualsTableTests.LeftTable (a INT, b INT);
+   INSERT INTO AssertEqualsTableTests.LeftTable VALUES (11, 12);
+   INSERT INTO AssertEqualsTableTests.LeftTable VALUES (31, 32);
+
+   CREATE TABLE AssertEqualsTableTests.RightTable (a INT, b INT);
+   INSERT INTO AssertEqualsTableTests.RightTable VALUES (11, 12);
+   INSERT INTO AssertEqualsTableTests.RightTable VALUES (21, 22);
+   
+   CREATE TABLE AssertEqualsTableTests.ResultTable ([_m_] CHAR(1), a INT, b INT);
+   INSERT INTO AssertEqualsTableTests.ResultTable ([_m_], a, b)
+   SELECT '=', 11, 12 UNION ALL
+   SELECT '<', 31, 32 UNION ALL
+   SELECT '>', 21, 22;
+   
+   DECLARE @ExpectedMessage NVARCHAR(MAX);
+   EXEC tSQLt.TableToText @TableName = 'AssertEqualsTableTests.ResultTable', @OrderBy = '_m_',@txt = @ExpectedMessage OUTPUT;
+   SET @ExpectedMessage = 'unexpected/missing resultset rows!'+CHAR(13)+CHAR(10)+@ExpectedMessage;
+
+   EXEC tSQLt_testutil.AssertFailMessageEquals 
+     'EXEC tSQLt.AssertEqualsTable ''AssertEqualsTableTests.LeftTable'', ''AssertEqualsTableTests.RightTable'';',
+     @ExpectedMessage,
+     'Fail was not called with expected message:';
+END;
+GO
+
+CREATE PROCEDURE AssertEqualsTableTests.[test multiple rows with one mismatching row with mismatching column values in last column]
+AS
+BEGIN
+   CREATE TABLE AssertEqualsTableTests.LeftTable (a INT, b INT);
+   INSERT INTO AssertEqualsTableTests.LeftTable VALUES (11, 199);
+
+   CREATE TABLE AssertEqualsTableTests.RightTable (a INT, b INT);
+   INSERT INTO AssertEqualsTableTests.RightTable VALUES (11, 12);
+   
+   CREATE TABLE AssertEqualsTableTests.ResultTable ([_m_] CHAR(1), a INT, b INT);
+   INSERT INTO AssertEqualsTableTests.ResultTable ([_m_], a, b)
+   SELECT '<', 11, 199 UNION ALL
+   SELECT '>', 11, 12;
+   
+   DECLARE @ExpectedMessage NVARCHAR(MAX);
+   EXEC tSQLt.TableToText @TableName = 'AssertEqualsTableTests.ResultTable', @OrderBy = '_m_',@txt = @ExpectedMessage OUTPUT;
+   SET @ExpectedMessage = 'unexpected/missing resultset rows!'+CHAR(13)+CHAR(10)+@ExpectedMessage;
+
+   EXEC tSQLt_testutil.AssertFailMessageEquals 
+     'EXEC tSQLt.AssertEqualsTable ''AssertEqualsTableTests.LeftTable'', ''AssertEqualsTableTests.RightTable'';',
+     @ExpectedMessage,
+     'Fail was not called with expected message:';
+END;
+GO
+
+CREATE PROCEDURE AssertEqualsTableTests.[test multiple rows with one mismatching row with mismatching column values in first column]
+AS
+BEGIN
+   CREATE TABLE AssertEqualsTableTests.LeftTable (a INT, b INT);
+   INSERT INTO AssertEqualsTableTests.LeftTable VALUES (199, 12);
+
+   CREATE TABLE AssertEqualsTableTests.RightTable (a INT, b INT);
+   INSERT INTO AssertEqualsTableTests.RightTable VALUES (11, 12);
+   
+   CREATE TABLE AssertEqualsTableTests.ResultTable ([_m_] CHAR(1), a INT, b INT);
+   INSERT INTO AssertEqualsTableTests.ResultTable ([_m_], a, b)
+   SELECT '<', 199, 12 UNION ALL
+   SELECT '>', 11, 12;
+   
+   DECLARE @ExpectedMessage NVARCHAR(MAX);
+   EXEC tSQLt.TableToText @TableName = 'AssertEqualsTableTests.ResultTable', @OrderBy = '_m_',@txt = @ExpectedMessage OUTPUT;
+   SET @ExpectedMessage = 'unexpected/missing resultset rows!'+CHAR(13)+CHAR(10)+@ExpectedMessage;
+
+   EXEC tSQLt_testutil.AssertFailMessageEquals 
+     'EXEC tSQLt.AssertEqualsTable ''AssertEqualsTableTests.LeftTable'', ''AssertEqualsTableTests.RightTable'';',
+     @ExpectedMessage,
+     'Fail was not called with expected message:';
+END;
+GO
+
+--- At this point, AssertEqualsTable is tested enough we feel confident in using it in the remaining tests ---
+
 CREATE PROCEDURE AssertEqualsTableTests.[test multiple rows with multiple mismatching rows]
 AS
 BEGIN
@@ -188,26 +310,22 @@ BEGIN
    INSERT INTO AssertEqualsTableTests.RightTable VALUES (2);
    INSERT INTO AssertEqualsTableTests.RightTable VALUES (4);
    
-   CREATE TABLE AssertEqualsTableTests.ResultTable ([_m_] CHAR(1),i INT);
-   INSERT INTO AssertEqualsTableTests.ResultTable ([_m_],i)
+   CREATE TABLE AssertEqualsTableTests.ExpectedResultTable ([_m_] CHAR(1),i INT);
+   INSERT INTO AssertEqualsTableTests.ExpectedResultTable ([_m_],i)
    SELECT '=',1 UNION ALL
    SELECT '>',2 UNION ALL
    SELECT '<',3 UNION ALL
    SELECT '>',4 UNION ALL
    SELECT '<',5;
    
-   DECLARE @ExpectedMessage NVARCHAR(MAX);
-   EXEC tSQLt.TableToText @TableName = 'AssertEqualsTableTests.ResultTable', @OrderBy = '_m_',@txt = @ExpectedMessage OUTPUT;
-   SET @ExpectedMessage = 'unexpected/missing resultset rows!'+CHAR(13)+CHAR(10)+@ExpectedMessage;
-
-   EXEC tSQLt_testutil.AssertFailMessageEquals 
-     'EXEC tSQLt.AssertEqualsTable ''AssertEqualsTableTests.LeftTable'', ''AssertEqualsTableTests.RightTable'';',
-     @ExpectedMessage,
-     'Fail was not called with expected message:';
+   CREATE TABLE AssertEqualsTableTests.ActualResultTable ([_m_] CHAR(1),i INT);
+   EXEC tSQLt.Private_CompareTables 'AssertEqualsTableTests.LeftTable', 'AssertEqualsTableTests.RightTable', 'AssertEqualsTableTests.ActualResultTable', 'i', '_m_';
+   
+   EXEC tSQLt.AssertEqualsTable 'AssertEqualsTableTests.ExpectedResultTable', 'AssertEqualsTableTests.ActualResultTable';
 END;
 GO
 
-CREATE PROCEDURE AssertEqualsTableTests.[test multiple ]
+CREATE PROCEDURE AssertEqualsTableTests.[test same row in each table but different row counts]
 AS
 BEGIN
    CREATE TABLE AssertEqualsTableTests.LeftTable (i INT);
@@ -223,6 +341,50 @@ BEGIN
    INSERT INTO AssertEqualsTableTests.ResultTable ([_m_],i)
    SELECT '=',1 UNION ALL
    SELECT '=',3 UNION ALL
+   SELECT '<',3;
+   
+   DECLARE @ExpectedMessage NVARCHAR(MAX);
+   EXEC tSQLt.TableToText @TableName = 'AssertEqualsTableTests.ResultTable', @OrderBy = '_m_',@txt = @ExpectedMessage OUTPUT;
+   SET @ExpectedMessage = 'unexpected/missing resultset rows!'+CHAR(13)+CHAR(10)+@ExpectedMessage;
+
+   EXEC tSQLt_testutil.AssertFailMessageEquals 
+     'EXEC tSQLt.AssertEqualsTable ''AssertEqualsTableTests.LeftTable'', ''AssertEqualsTableTests.RightTable'';',
+     @ExpectedMessage,
+     'Fail was not called with expected message:';
+END;
+GO
+
+CREATE PROCEDURE AssertEqualsTableTests.[test same row in each table but different row counts with more rows]
+AS
+BEGIN
+   CREATE TABLE AssertEqualsTableTests.LeftTable (i INT);
+   INSERT INTO AssertEqualsTableTests.LeftTable VALUES (1);
+   INSERT INTO AssertEqualsTableTests.LeftTable VALUES (1);
+   INSERT INTO AssertEqualsTableTests.LeftTable VALUES (3);
+   INSERT INTO AssertEqualsTableTests.LeftTable VALUES (3);
+   INSERT INTO AssertEqualsTableTests.LeftTable VALUES (3);
+   INSERT INTO AssertEqualsTableTests.LeftTable VALUES (3);
+   INSERT INTO AssertEqualsTableTests.LeftTable VALUES (3);
+
+   CREATE TABLE AssertEqualsTableTests.RightTable (i INT);
+   INSERT INTO AssertEqualsTableTests.RightTable VALUES (1);
+   INSERT INTO AssertEqualsTableTests.RightTable VALUES (1);
+   INSERT INTO AssertEqualsTableTests.RightTable VALUES (1);
+   INSERT INTO AssertEqualsTableTests.RightTable VALUES (1);
+   INSERT INTO AssertEqualsTableTests.RightTable VALUES (3);
+   INSERT INTO AssertEqualsTableTests.RightTable VALUES (3);
+   INSERT INTO AssertEqualsTableTests.RightTable VALUES (3);
+   
+   CREATE TABLE AssertEqualsTableTests.ResultTable ([_m_] CHAR(1),i INT);
+   INSERT INTO AssertEqualsTableTests.ResultTable ([_m_],i)
+   SELECT '=',1 UNION ALL
+   SELECT '=',1 UNION ALL
+   SELECT '>',1 UNION ALL
+   SELECT '>',1 UNION ALL
+   SELECT '=',3 UNION ALL
+   SELECT '=',3 UNION ALL
+   SELECT '=',3 UNION ALL
+   SELECT '<',3 UNION ALL
    SELECT '<',3;
    
    DECLARE @ExpectedMessage NVARCHAR(MAX);
