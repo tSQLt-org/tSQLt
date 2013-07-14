c1e1b931cc0b7a5d99056acbc6b18840e63ea420
maibotian <maibotian@91d956da-2944-4949-a11c-b487c32f045b>
2013-07-14 22:49:00 +0000

    finished NAnt build scripts

--- Build/localNbuild.bat
+++ Build/localNbuild.bat
@@ -1,1 +1,14 @@
-cmd /C %1\bin\nant -D:msbuild.path=%2 -D:db.server=%3 -D:db.name=%4 %5
+@ECHO OFF
+%1\bin\nant -buildfile:tSQLt.build -D:msbuild.path=%2 || goto :error
+%1\bin\nant -buildfile:tSQLt.validatebuild -D:db.version=%3 -D:db.server=%4 -D:db.name=%5 || goto :error
+
+ECHO +-----------------+
+ECHO : BUILD SUCCEEDED :
+ECHO +-----------------+
+goto :EOF
+
+:error
+ECHO +--------------+
+ECHO : BUILD FAILED :
+ECHO +--------------+
+exit /b %errorlevel%
