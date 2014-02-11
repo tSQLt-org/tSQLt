9419c5baa65c9e72ca73c6d684d3064ccf796d95
maibotian <maibotian@91d956da-2944-4949-a11c-b487c32f045b>
2014-02-11 18:09:10 +0000

    Added tSQLt.AssertObjectDoesNotExist

---
+++ Source/tSQLt.AssertObjectDoesNotExist.ssp.sql
@@ -0,0 +1,19 @@
+IF OBJECT_ID('tSQLt.AssertObjectDoesNotExist') IS NOT NULL DROP PROCEDURE tSQLt.AssertObjectDoesNotExist;
+GO
+---Build+
+CREATE PROCEDURE tSQLt.AssertObjectDoesNotExist
+    @ObjectName NVARCHAR(MAX),
+    @Message NVARCHAR(MAX) = NULL
+AS
+BEGIN
+     DECLARE @Msg NVARCHAR(MAX);
+     IF OBJECT_ID(@ObjectName) IS NOT NULL
+     OR(@ObjectName LIKE '#%' AND OBJECT_ID('tempdb..'+@ObjectName) IS NOT NULL)
+     BEGIN
+         SELECT @Msg = '''' + @ObjectName + ''' does exist!';
+         EXEC tSQLt.Fail @Message,@Msg;
+     END;
+END;
+---Build-
+GO
+
