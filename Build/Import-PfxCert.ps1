751c28deb4258b0813f6076a596e78a93a2cc731
mbt1 <bitbucket@meine.us>
2018-03-22 11:47:06 -0400

    replaced &quot; with actual "

--- Build/Import-PfxCert.ps1
+++ Build/Import-PfxCert.ps1
@@ -1,13 +1,13 @@
 param($PfxFilePath, $Password)
  
 $absolutePfxFilePath = Resolve-Path -Path $PfxFilePath
-Write-Output &quot;Importing store certificate &#39;$absolutePfxFilePath&#39;...&quot;
+Write-Output "Importing store certificate &#39;$absolutePfxFilePath&#39;..."
  
 Add-Type -AssemblyName System.Security
 $cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
 $cert.Import($absolutePfxFilePath, $Password, [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::PersistKeySet)
-$store = new-object system.security.cryptography.X509Certificates.X509Store -argumentlist &quot;MY&quot;, CurrentUser
-$store.Open([System.Security.Cryptography.X509Certificates.OpenFlags]::&quot;ReadWrite&quot;)
+$store = new-object system.security.cryptography.X509Certificates.X509Store -argumentlist "MY", CurrentUser
+$store.Open([System.Security.Cryptography.X509Certificates.OpenFlags]::"ReadWrite")
 $store.Add($cert)
 $store.Close()
 
