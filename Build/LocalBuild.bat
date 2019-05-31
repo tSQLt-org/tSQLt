4b4fc26d55f64967044b4a6424b682fc570c21c5
mbt1 <bitbucket@meine.us>
2019-05-31 10:38:25 -0400

    Started conversion from NANT to ANT

--- Build/LocalBuild.bat
+++ Build/LocalBuild.bat
@@ -1,1 +1,30 @@
-cmd /C %1\bin\ant -Dmsbuild.path=%2 -Ddb.server=%3 -Ddb.name=%4 %5
+@ECHO OFF
+
+ECHO +-------------------+
+ECHO : Starting BUILD    :
+ECHO +-------------------+
+REM CALL %1\bin\ant -buildfile tSQLt.build.xml -Dmsbuild.path=%2 || goto :error
+
+ECHO +-------------------+
+ECHO : Copying BUILD     :
+ECHO +-------------------+
+REM CALL %1\bin\ant -buildfile tSQLt.copybuild.xml -debug|| goto :error
+
+ECHO +-------------------+
+ECHO : Validating BUILD  :
+ECHO +-------------------+
+CALL %1\bin\ant -buildfile tSQLt.validatebuild.xml -Ddb.version=%3 -Ddb.server=%4 -Ddb.name=%5 -debug|| goto :error
+
+ECHO +-------------------+
+ECHO : BUILD SUCCEEDED   :
+ECHO +-------------------+
+goto :EOF
+
+:error
+ECHO +-------------------+
+ECHO : BUILD FAILED      :
+ECHO +-------------------+
+exit /b %errorlevel%
+
+
+REM Override ignored for property
