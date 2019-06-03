1d84bf92e0a57aad7c945c462932d4af974efc9b
mbt1 <bitbucket@meine.us>
2019-06-03 08:59:20 -0400

    ANT Build succeeds

--- Build/LocalBuild.bat
+++ Build/LocalBuild.bat
@@ -1,5 +1,8 @@
 @ECHO OFF
 
+REM CALL %1\bin\ant -buildfile tSQLt.experiments.build.xml -Dmsbuild.path=%2 || goto :error
+REM goto :EOF
+
 ECHO +-------------------+
 ECHO : Starting BUILD    :
 ECHO +-------------------+
@@ -8,12 +11,12 @@ CALL %1\bin\ant -buildfile tSQLt.build.xml -Dmsbuild.path=%2 || goto :error
 ECHO +-------------------+
 ECHO : Copying BUILD     :
 ECHO +-------------------+
-CALL %1\bin\ant -buildfile tSQLt.copybuild.xml -debug|| goto :error
+CALL %1\bin\ant -buildfile tSQLt.copybuild.xml || goto :error
 
 ECHO +-------------------+
 ECHO : Validating BUILD  :
 ECHO +-------------------+
-CALL %1\bin\ant -buildfile tSQLt.validatebuild.xml -Ddb.version=%3 -Ddb.server=%4 -Ddb.name=%5|| goto :error
+CALL %1\bin\ant -buildfile tSQLt.validatebuild.xml -Ddb.version=%3 -Ddb.server=%4 -Ddb.name=%5 || goto :error
 
 ECHO +-------------------+
 ECHO : BUILD SUCCEEDED   :
