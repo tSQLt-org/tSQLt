d3cb6eabb05c40d0e170738a4ed744b2fa1b8f17
mbt1 <bitbucket@meine.us>
2019-06-04 17:13:55 -0400

    selective CLR signing

--- Build/LocalBuild.bat
+++ Build/LocalBuild.bat
@@ -3,31 +3,34 @@
 REM CALL %1\bin\ant -buildfile tSQLt.experiments.build.xml -Dmsbuild.path=%2 || goto :error
 REM goto :EOF
 
-ECHO +-------------------+
-ECHO : Starting BUILD    :
-ECHO +-------------------+
-CALL %1\bin\ant -buildfile tSQLt.build.xml -Dmsbuild.path=%2 || goto :error
+ECHO +-------------------------+
+ECHO : Starting CLR BUILD      :
+ECHO +-------------------------+
+CALL %1\bin\ant -buildfile tSQLt.buildCLR.xml -Dmsbuild.path=%2 || goto :error
 
-ECHO +-------------------+
-ECHO : Copying BUILD     :
-ECHO +-------------------+
+ECHO +-------------------------+
+ECHO : Starting tSQLt BUILD    :
+ECHO +-------------------------+
+CALL %1\bin\ant -buildfile tSQLt.build.xml || goto :error
+
+ECHO +-------------------------+
+ECHO : Copying BUILD           :
+ECHO +-------------------------+
 CALL %1\bin\ant -buildfile tSQLt.copybuild.xml || goto :error
 
-ECHO +-------------------+
-ECHO : Validating BUILD  :
-ECHO +-------------------+
+ECHO +-------------------------+
+ECHO : Validating BUILD        :
+ECHO +-------------------------+
 CALL %1\bin\ant -buildfile tSQLt.validatebuild.xml -Ddb.version=%3 -Ddb.server=%4 -Ddb.name=%5 || goto :error
 
-ECHO +-------------------+
-ECHO : BUILD SUCCEEDED   :
-ECHO +-------------------+
+ECHO +-------------------------+
+ECHO :     BUILD SUCCEEDED     :
+ECHO +-------------------------+
 goto :EOF
 
 :error
-ECHO +-------------------+
-ECHO : BUILD FAILED      :
-ECHO +-------------------+
+ECHO +-------------------------+
+ECHO :  !!! BUILD FAILED !!!   :
+ECHO +-------------------------+
 exit /b %errorlevel%
 
-
-REM Override ignored for property
