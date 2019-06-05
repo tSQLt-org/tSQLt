0e84ae300cfac86d331f2f01db01f4ce3ea833dd
mbt1 <bitbucket@meine.us>
2019-06-05 17:12:59 -0400

    local build successful (including signing) - switched to CruiseControl for local build - switched unzip to copyfile with archive fileset

--- Build/LocalBuild.bat
+++ Build/LocalBuild.bat
@@ -1,27 +1,27 @@
 @ECHO OFF
 
-REM CALL %1\bin\ant -buildfile tSQLt.experiments.build.xml -Dmsbuild.path=%2 || goto :error
+REM CALL %1\bin\ant -buildfile Build\tSQLt.experiments.build.xml -Dmsbuild.path=%2 || goto :error
 REM goto :EOF
 
 ECHO +-------------------------+
 ECHO : Starting CLR BUILD      :
 ECHO +-------------------------+
-CALL %1\bin\ant -buildfile tSQLt.buildCLR.xml -Dmsbuild.path=%2 || goto :error
+CALL %1\bin\ant -buildfile Build\tSQLt.buildCLR.xml -Dmsbuild.path=%2 || goto :error
 
 ECHO +-------------------------+
 ECHO : Starting tSQLt BUILD    :
 ECHO +-------------------------+
-CALL %1\bin\ant -buildfile tSQLt.build.xml || goto :error
+CALL %1\bin\ant -buildfile Build\tSQLt.build.xml || goto :error
 
 ECHO +-------------------------+
 ECHO : Copying BUILD           :
 ECHO +-------------------------+
-CALL %1\bin\ant -buildfile tSQLt.copybuild.xml || goto :error
+CALL %1\bin\ant -buildfile Build\tSQLt.copybuild.xml || goto :error
 
 ECHO +-------------------------+
 ECHO : Validating BUILD        :
 ECHO +-------------------------+
-CALL %1\bin\ant -buildfile tSQLt.validatebuild.xml -Ddb.version=%3 -Ddb.server=%4 -Ddb.name=%5 || goto :error
+CALL %1\bin\ant -buildfile Build\tSQLt.validatebuild.xml -Ddb.version=%3 -Ddb.server=%4 -Ddb.name=%5 || goto :error
 
 ECHO +-------------------------+
 ECHO :     BUILD SUCCEEDED     :
