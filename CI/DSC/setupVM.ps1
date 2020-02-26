e6d025b0b31247cd57b1d4a2c1eba39ae8a63b03
mbt1 <bitbucket@meine.us>
2020-02-26 10:31:16 -0500

    switched to Invoke-SqlCmd to execute as different user

--- CI/DSC/setupVM.ps1
+++ CI/DSC/setupVM.ps1
@@ -1,17 +1,32 @@
 Function Setup-VM() {
 
-    Write-Host 'Setting up VM for tSQLt CI execution...'
+    Write-Host "Starting..."
+	Write-Host "Executed as: $(whoami)"
+	Write-Host 'Setting up VM for tSQLt CI execution...'
+	
 	#<#
 	New-NetFirewallRule -DisplayName "Allow WinRM" -Direction Inbound -LocalPort 5986 -Protocol TCP -Action Allow
 	New-NetFirewallRule -DisplayName "Allow SQLServer" -Direction Inbound -LocalPort 1433 -Protocol TCP -Action Allow
 
-	SqlCmd -S localhost -E -Q "PRINT @@VERSION;"
-	SqlCmd -S localhost -E -Q "CREATE LOGIN tSQLtExternal WITH PASSWORD = 'qTv3f9gduUFuc8BQTZUq4MEcFbY3(2H)',CHECK_EXPIRATION = OFF, CHECK_POLICY = OFF,DEFAULT_DATABASE = tempdb;ALTER SERVER ROLE sysadmin ADD MEMBER tSQLtExternal;"
+    $User = "tSQLt"
+	$PWord = ConvertTo-SecureString -String "qTv3f9gduUFuc8BQTZUq4MEcFbY3(2H)" -AsPlainText -Force
+    $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
 
-	SqlCmd -S localhost -E -Q "EXEC xp_instance_regwrite N'HKEY_LOCAL_MACHINE', N'Software\Microsoft\MSSQLServer\MSSQLServer', N'LoginMode', REG_DWORD, 2"
+	(Invoke-SqlCmd -Verbose -Credential $Credential -Query "PRINT @@VERSION;" )[0]
+	(Invoke-SqlCmd -Verbose -Credential $Credential -Query "CREATE LOGIN tSQLtExternal WITH PASSWORD = 'qTv3f9gduUFuc8BQTZUq4MEcFbY3(2H)',CHECK_EXPIRATION = OFF, CHECK_POLICY = OFF,DEFAULT_DATABASE = tempdb;ALTER SERVER ROLE sysadmin ADD MEMBER tSQLtExternal;")[0]
+
+	(Invoke-SqlCmd -Verbose -Credential $Credential -Query "EXEC xp_instance_regwrite N'HKEY_LOCAL_MACHINE', N'Software\Microsoft\MSSQLServer\MSSQLServer', N'LoginMode', REG_DWORD, 2")[0]
 
 	Restart-Service -Name "SQL Server (MSSQLSERVER)"
     ##>
+
     Write-Host 'Done setting up VM for tSQLt CI execution.'
 }
 Setup-VM
+
+
+
+
+$User = "Domain01\User01"
+PS> $PWord = ConvertTo-SecureString -String "P@sSwOrd" -AsPlainText -Force
+PS> $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
