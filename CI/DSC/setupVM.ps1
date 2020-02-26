8194cc361a9f2aefd754425a9bd8ca8f195a8313
mbt1 <bitbucket@meine.us>
2020-02-26 11:46:51 -0500

    included Invoke-Command test

--- CI/DSC/setupVM.ps1
+++ CI/DSC/setupVM.ps1
@@ -9,13 +9,16 @@ Function Setup-VM() {
 	New-NetFirewallRule -DisplayName "Allow SQLServer" -Direction Inbound -LocalPort 1433 -Protocol TCP -Action Allow
 
     $User = "tSQLt"
-	$PWord = ConvertTo-SecureString -String "qTv3f9gduUFuc8BQTZUq4MEcFbY3(2H)" -AsPlainText -Force
-    $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
+	$Pword = "qTv3f9gduUFuc8BQTZUq4MEcFbY3(2H)"
+	$PWordS = ConvertTo-SecureString -String $Pword -AsPlainText -Force
+    $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWordS
 
-	(Invoke-SqlCmd -Verbose -Credential $Credential -Query "PRINT @@VERSION;" )[0]
-	(Invoke-SqlCmd -Verbose -Credential $Credential -Query "CREATE LOGIN tSQLtExternal WITH PASSWORD = 'qTv3f9gduUFuc8BQTZUq4MEcFbY3(2H)',CHECK_EXPIRATION = OFF, CHECK_POLICY = OFF,DEFAULT_DATABASE = tempdb;ALTER SERVER ROLE sysadmin ADD MEMBER tSQLtExternal;")[0]
+	Invoke-Command -ComputerName localhost  -ScriptBlock {Write-Host $(whoami)} -Credential $Credential
+	#Invoke-Command -ComputerName localhost  -ScriptBlock {Write-Host h:$using:hello} -Credential $Credential
+	#(Invoke-SqlCmd -Verbose -Credential $Credential -Query "PRINT @@VERSION;" )[0]
+	#(Invoke-SqlCmd -Verbose -Credential $Credential -Query "CREATE LOGIN tSQLtExternal WITH PASSWORD = 'qTv3f9gduUFuc8BQTZUq4MEcFbY3(2H)',CHECK_EXPIRATION = OFF, CHECK_POLICY = OFF,DEFAULT_DATABASE = tempdb;ALTER SERVER ROLE sysadmin ADD MEMBER tSQLtExternal;")[0]
 
-	(Invoke-SqlCmd -Verbose -Credential $Credential -Query "EXEC xp_instance_regwrite N'HKEY_LOCAL_MACHINE', N'Software\Microsoft\MSSQLServer\MSSQLServer', N'LoginMode', REG_DWORD, 2")[0]
+	#(Invoke-SqlCmd -Verbose -Credential $Credential -Query "EXEC xp_instance_regwrite N'HKEY_LOCAL_MACHINE', N'Software\Microsoft\MSSQLServer\MSSQLServer', N'LoginMode', REG_DWORD, 2")[0]
 
 	Restart-Service -Name "SQL Server (MSSQLSERVER)"
     ##>
@@ -24,9 +27,3 @@ Function Setup-VM() {
 }
 Setup-VM
 
-
-
-
-$User = "Domain01\User01"
-PS> $PWord = ConvertTo-SecureString -String "P@sSwOrd" -AsPlainText -Force
-PS> $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
