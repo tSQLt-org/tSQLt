abb2f81aa90bf6ed7634df6231fc5aba491c1bbf
mbt1 <bitbucket@meine.us>
2020-03-20 11:19:51 -0400

    changed GetLabVMParams.ps1 to take resourcegroupname and vmname instead of labvmid

--- CI/GetLabVMParams.ps1
+++ CI/GetLabVMParams.ps1
@@ -1,4 +1,4 @@
-Param( [string] $labVmId)
+Param( [string] $ResourceGroupName, [string] $VMName)
 
 #$labVmId = "/subscriptions/58c04a99-5b92-410c-9e41-10262f68ca80/resourceGroups/tSQLtCI_DevTestLab_3_RG/providers/Microsoft.DevTestLab/labs/tSQLtCI_DevTestLab_3/virtualmachines/SQL2014SP3D"
 
@@ -17,11 +17,12 @@ Write-Host $env:UserDomain
 Write-Host $env:ComputerName
 Write-Host "<-><-><-><-><-><-><-><-><-><-><-><-><-><->";
 
-$labAzResource = (Get-AzResource -Id $labVmId)
+$labAzResource = (Get-AzResource -Name $VMName -ResourceType Microsoft.DevTestLab/labs/virtualmachines -ResourceGroupName $ResourceGroupName)
+##Get-AzResource -Name "V1062sql2014sp3" -ResourceType "Microsoft.DevTestLab/labs/virtualmachines" -ResourceGroupName "tSQLtCI_DevTestLab_20200320_1062"
 
 Write-Host $labAzResource.Properties.toString()
 
-$labVmComputeId = (Get-AzResource -Id $labVmId).Properties.ComputeId
+$labVmComputeId = $labAzResource.Properties.ComputeId
 Write-Host "setting variable: labVmComputeId:" $labVmComputeId
 Write-Host "##vso[task.setvariable variable=labVmComputeId;]$labVmComputeId"
 
@@ -29,14 +30,20 @@ $labVmRgName = (Get-AzResource -Id $labVmComputeId).ResourceGroupName
 Write-Host "setting variable: labVmRgName:" $labVmRgName
 Write-Host "##vso[task.setvariable variable=labVmRgName;]$labVmRgName"
 
-$labVmName = (Get-AzResource -Id $labVmId).Name
-Write-Host "setting labVmName: labVMIpAddress:" $labVMIpAddress
+$labVmName = $labAzResource.Name
+Write-Host "setting variable: labVmName:" $labVmName
 Write-Host "##vso[task.setvariable variable=labVmName;]$labVmName"
 
-$labVMIpAddress = (Get-AzPublicIpAddress -ResourceGroupName $labVmRgName -Name $labVmName).IpAddress
+$labVMId = $labAzResource.ResourceId
+Write-Host 'labVMId: ' $labVMId
+Write-Host "##vso[task.setvariable variable=labVMId;]$labVMId"
+
+$PublicIpAddress= (Get-AzPublicIpAddress -ResourceGroupName $labVmRgName -Name $labVmName)
+$labVMIpAddress = $PublicIpAddress.IpAddress
 Write-Host "setting variable: labVMIpAddress:" $labVMIpAddress
 Write-Host "##vso[task.setvariable variable=labVMIpAddress;]$labVMIpAddress"
 
-$labVMFqdn = (Get-AzPublicIpAddress -ResourceGroupName $labVmRgName -Name $labVmName).DnsSettings.Fqdn
+$labVMFqdn = $PublicIpAddress.DnsSettings.Fqdn
 Write-Host "setting variable: labVMFqdn:" $labVMFqdn
 Write-Host "##vso[task.setvariable variable=labVMFqdn;]$labVMFqdn"
+
