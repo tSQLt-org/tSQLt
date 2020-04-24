d9f555703a679508096fcb315e950c3537b2a8ec
mbt1 <bitbucket@meine.us>
2020-04-24 10:58:40 -0400

    added sqlcmdpath to the build

--- Build/LocalBuild.bat
+++ Build/LocalBuild.bat
@@ -1,31 +1,48 @@
 @ECHO OFF
 
-REM CALL %1\bin\ant -buildfile Build\tSQLt.experiments.build.xml -Dmsbuild.path=%2 -verbose || goto :error
+ECHO +-------------------------+
+ECHO : Executing Local Build   :
+ECHO +-------------------------+
+ECHO Parameters:
+ECHO AntHome: "%~1"
+ECHO NET4Home: "%~2"
+ECHO SQLCMDPath: "%~3"
+ECHO SQLVersion: "%~4"
+ECHO SQLInstanceName: "%~5"
+ECHO DBName: "%~6"
+
+REM CALL "%~1\bin\ant" -buildfile Build\tSQLt.experiments.build.xml -Dmsbuild.path="%~2" -verbose || goto :error
 REM goto :EOF
 
 ECHO +-------------------------+
 ECHO : Starting CLR BUILD      :
 ECHO +-------------------------+
-CALL %1\bin\ant -buildfile Build\tSQLt.buildCLR.xml -Dmsbuild.path=%2 || goto :error
+IF "%~7"=="-v" @ECHO ON
+CALL "%~1\bin\ant" -buildfile Build\tSQLt.buildCLR.xml -Dmsbuild.path="%~2" || goto :error
+@ECHO OFF
 
 ECHO +-------------------------+
 ECHO : Starting tSQLt BUILD    :
 ECHO +-------------------------+
-CALL %1\bin\ant -buildfile Build\tSQLt.build.xml || goto :error
+IF "%~7"=="-v" @ECHO ON
+CALL "%~1\bin\ant" -buildfile Build\tSQLt.build.xml || goto :error
+@ECHO OFF
 
 ECHO +-------------------------+
 ECHO : Copying BUILD           :
 ECHO +-------------------------+
 ECHO :- THIS STEP IS OPTIONAL -:
 ECHO +-------------------------+
-CALL %1\bin\ant -buildfile Build\tSQLt.copybuild.xml || goto :error
-
-pause
+IF "%~7"=="-v" @ECHO ON
+CALL "%~1\bin\ant" -buildfile Build\tSQLt.copybuild.xml || goto :error
+@ECHO OFF
 
 ECHO +-------------------------+
 ECHO : Validating BUILD        :
 ECHO +-------------------------+
-CALL %1\bin\ant -buildfile Build\tSQLt.validatebuild.xml -Ddb.version=%3 -Ddb.server=%4 -Ddb.name=%5 -Ddb.login="-E" || goto :error
+IF "%~7"=="-v" @ECHO ON
+CALL "%~1\bin\ant" -buildfile Build\tSQLt.validatebuild.xml -Ddb.version=%4 -Ddb.server=%5 -Ddb.name=%~6 -Ddb.login="-E" -Dsqlcmd.path="\"%~3\"" || goto :error
+@ECHO OFF
 
 ECHO +-------------------------+
 ECHO :     BUILD SUCCEEDED     :
--- Source/tSQLt.class.sql
+++ Source/tSQLt.class.sql
@@ -59,7 +59,8 @@ CREATE PROCEDURE tSQLt.Private_PrintXML
     @Message XML
 AS 
 BEGIN
-    SELECT @Message FOR XML PATH(''), TYPE;--Required together with ":XML ON" sqlcmd statement to allow more than 1mb to be returned
+    SET NOCOUNT ON;
+    SELECT CAST(@Message AS XML);--Required together with ":XML ON" sqlcmd statement to allow more than 1mb to be returned
     RETURN 0;
 END;
 GO
