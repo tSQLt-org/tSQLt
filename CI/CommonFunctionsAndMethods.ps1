165c6fd3c9d0b277c8c48140048adf12e4ab0d82
mbt1 <bitbucket@meine.us>
2020-06-09 10:10:05 -0400

    Invoke() returns a collection of results. Added [0] to deal with that.

--- CI/CommonFunctionsAndMethods.ps1
+++ CI/CommonFunctionsAndMethods.ps1
@@ -1,2 +1,7 @@
 ﻿$MergeHashTables = {param([HashTable]$base,[HashTable]$new);$new.GetEnumerator()|%{$base.remove($_.Key);$base += @{$_.Key=$_.Value}};$base;}
-$AddTagsToResourceGroup = {param([String]$ResourceGroupName,[HashTable]$newTags);$RG = Get-AzResourceGroup -name $ResourceGroupName;$RG|SetAzResourceGroup -Tags $MergeHashTables.Invoke($RG.Tags,$newTags);}
+$AddTagsToResourceGroup = 
+{
+  param([String]$ResourceGroupName,[HashTable]$newTags);
+  $RG = Get-AzResourceGroup -name $ResourceGroupName;
+  $RG|Set-AzResourceGroup -Tags ($MergeHashTables.Invoke($RG.Tags,$newTags)[0]);
+}
