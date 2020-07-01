49f2133c7e5ec4d47c7109ed1b2b33183720c715
Sebastian <github@meine.us>
2020-07-01 11:26:57 -0400

    Change of logging - step 1

--- CI/CreateVM.ps1
+++ CI/CreateVM.ps1
@@ -17,24 +17,24 @@ Param(
 
 $scriptpath = $MyInvocation.MyCommand.Path
 $dir = Split-Path $scriptpath
-Write-host "FileLocation: $dir"
+Write-Verbose "FileLocation: $dir"
 
 .($dir+"\CommonFunctionsAndMethods.ps1")
-Write-host $GetUTCTimeStamp.Invoke()"Common Functions Imported."
+Write-Verbose $GetUTCTimeStamp.Invoke()"Common Functions Imported."
 
 
-Write-Host "<->1<-><-><-><-><-><-><-><-><-><-><-><-><->";
-Write-Host "Parameters:";
-Write-Host "NamePreFix:" $NamePreFix;
-Write-Host "BuildId:" $BuildId;
-Write-Host "SQLVersionEdition:" $SQLVersionEdition;
-Write-Host "<->2<-><-><-><-><-><-><-><-><-><-><-><-><->";
-Write-Host "Execution Environment"
-Write-Host "UserName:"     $env:UserName
-Write-Host "UserDomain:"   $env:UserDomain
-Write-Host "ComputerName:" $env:ComputerName
-Write-Host "<->3<-><-><-><-><-><-><-><-><-><-><-><-><->";
-Write-Host $GetUTCTimeStamp.Invoke();
+Write-Verbose "<->1<-><-><-><-><-><-><-><-><-><-><-><-><->";
+Write-Verbose "Parameters:";
+Write-Verbose "NamePreFix:" $NamePreFix;
+Write-Verbose "BuildId:" $BuildId;
+Write-Verbose "SQLVersionEdition:" $SQLVersionEdition;
+Write-Verbose "<->2<-><-><-><-><-><-><-><-><-><-><-><-><->";
+Write-Verbose "Execution Environment"
+Write-Verbose "UserName:"     $env:UserName
+Write-Verbose "UserDomain:"   $env:UserDomain
+Write-Verbose "ComputerName:" $env:ComputerName
+Write-Verbose "<->3<-><-><-><-><-><-><-><-><-><-><-><-><->";
+Write-Verbose $GetUTCTimeStamp.Invoke();
 
 #####################
 $DTLName = ("$NamePreFix" + (Get-Date).tostring('yyyyMMdd') + "_" + $SQLVersionEdition + "_" + $BuildId)
@@ -43,20 +43,20 @@ $DTLVNetName = $DTLName+'_VNet0001'
 $DTLVmName = ("V{0}-{1}###############" -f $BuildId,$SQLVersionEdition).substring(0,15).replace('#','')
 
 #[string] $DTLRGName, [string] $DTLName, [string] $DTLVmName, [string] $DTLVNetName, [string] $DTLVNetSubnetName
-Write-Host "<->4<-><-><-><-><-><-><-><-><-><-><-><-><->";
-Write-Host "Names:";
-Write-Host "DTLRGName:" $DTLRGName;
-Write-Host "DTLName:" $DTLName;
-Write-Host "DTLVmName:" $DTLVmName;
-Write-Host "DTLVNetName:" $DTLVNetName;
-Write-Host "<->5<-><-><-><-><-><-><-><-><-><-><-><-><->";
+Write-Verbose "<->4<-><-><-><-><-><-><-><-><-><-><-><-><->";
+Write-Verbose "Names:";
+Write-Verbose "DTLRGName:" $DTLRGName;
+Write-Verbose "DTLName:" $DTLName;
+Write-Verbose "DTLVmName:" $DTLVmName;
+Write-Verbose "DTLVNetName:" $DTLVNetName;
+Write-Verbose "<->5<-><-><-><-><-><-><-><-><-><-><-><-><->";
 
 
-Write-Host $GetUTCTimeStamp.Invoke()"Creating Resource Group $DTLRGName"
+Write-Verbose $GetUTCTimeStamp.Invoke()"Creating Resource Group $DTLRGName"
 New-AzResourceGroup -Name "$DTLRGName" -Location "East US 2" -Tag @{Department="tSQLtCI"; Ephemeral="True"} -Force
-Write-Host $GetUTCTimeStamp.Invoke()"DONE: Creating Resource Group $DTLRGName"
+Write-Verbose $GetUTCTimeStamp.Invoke()"DONE: Creating Resource Group $DTLRGName"
 
-Write-Host $GetUTCTimeStamp.Invoke()"Creating VNet $DTLVNetName"
+Write-Verbose $GetUTCTimeStamp.Invoke()"Creating VNet $DTLVNetName"
 $params = @{
     ResourceGroupName ="$DTLRGName";
     TemplateFile="$dir\CreateVNetTemplate.json";
@@ -66,10 +66,10 @@ $params = @{
 $VNet = New-AzResourceGroupDeployment @params;
 
 $DTLVNetSubnetName = $VNet.Outputs.subnetName.Value
-Write-Host "DTLVNetSubnetName:" $DTLVNetSubnetName;
-Write-Host $GetUTCTimeStamp.Invoke()"DONE: Creating VNet $DTLVNetName"
+Write-Verbose "DTLVNetSubnetName:" $DTLVNetSubnetName;
+Write-Verbose $GetUTCTimeStamp.Invoke()"DONE: Creating VNet $DTLVNetName"
 
-Write-Host $GetUTCTimeStamp.Invoke()"Creating DTL $DTLName"
+Write-Verbose $GetUTCTimeStamp.Invoke()"Creating DTL $DTLName"
 $params = @{
     ResourceGroupName="$DTLRGName";
     TemplateFile="$dir\CreateDevTestLabTemplate.json";
@@ -81,36 +81,36 @@ $params = @{
 };
 New-AzResourceGroupDeployment @params;
 
-Write-Host $GetUTCTimeStamp.Invoke()"DONE: Creating DTL $DTLName"
+Write-Verbose $GetUTCTimeStamp.Invoke()"DONE: Creating DTL $DTLName"
 
 #####################
 
 ##Set-Location $(Build.Repository.LocalPath)
-Write-Host $GetUTCTimeStamp.Invoke()'Creating New VM'
+Write-Verbose $GetUTCTimeStamp.Invoke()'Creating New VM'
 ##Set-PSDebug -Trace 1;
 $VMResourceGroupDeployment = New-AzResourceGroupDeployment -ResourceGroupName "$DTLRGName" -TemplateFile "$dir\CreateVMTemplate.json" -labName "$DTLName" -newVMName "$DTLVmName" -DevTestLabVirtualNetworkName "$DTLVNetName" -DevTestLabVirtualNetworkSubNetName "$DTLVNetSubnetName" -userName "$SQLUserName" -password "$SQLPassword" -ContactEmail "$LabShutdownNotificationEmail" -SQLVersionEdition "$SQLVersionEdition"
-Write-Host $GetUTCTimeStamp.Invoke()'Done: Creating New VM'
-Write-Host "+AA++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+Write-Verbose $GetUTCTimeStamp.Invoke()'Done: Creating New VM'
+Write-Verbose "+AA++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 $VMResourceGroupDeployment
-Write-Host "------"
+Write-Verbose "------"
 $VMResourceGroupDeployment.Outputs
-Write-Host "------"
+Write-Verbose "------"
 $SQLVersion = $VMResourceGroupDeployment.Outputs.sqlVersion.Value;
-Write-Host ("--->VMResourceGroupDeployment.Outputs.sqlVersion:{0}" -f $SQLVersion)
+Write-Verbose ("--->VMResourceGroupDeployment.Outputs.sqlVersion:{0}" -f $SQLVersion)
 
 $labVMId = $VMResourceGroupDeployment.Outputs.labVMId.Value;
-Write-Host ("--->VMResourceGroupDeployment.Outputs.vmId:{0}" -f $labVMId)
+Write-Verbose ("--->VMResourceGroupDeployment.Outputs.vmId:{0}" -f $labVMId)
 
 $VmComputeId = (Get-AzResource -id $labVMId).Properties.ComputeId;
-Write-Host ("--->VmComputeId:{0}" -f $VmComputeId)
-Write-Host "+BB++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+Write-Verbose ("--->VmComputeId:{0}" -f $VmComputeId)
+Write-Verbose "+BB++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 $ComputeRGN = (Get-AzResource -id $VmComputeId).ResourceGroupName
-Write-Host ("--->ComputeRGN:{0}" -f $ComputeRGN)
-Write-Host "+CC++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-Write-Host $GetUTCTimeStamp.Invoke()"Set Tags on ResourceGroup"
+Write-Verbose ("--->ComputeRGN:{0}" -f $ComputeRGN)
+Write-Verbose "+CC++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+Write-Verbose $GetUTCTimeStamp.Invoke()"Set Tags on ResourceGroup"
 Set-AzResourceGroup -Name $ComputeRGN -Tags @{"Department"="tSQLtCI";"ParentRGN"="$DTLRGName"}
-Write-Host $GetUTCTimeStamp.Invoke()"Done: Set Tags on ResourceGroup"
-Write-Host "+DD++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+Write-Verbose $GetUTCTimeStamp.Invoke()"Done: Set Tags on ResourceGroup"
+Write-Verbose "+DD++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 
 ##Set-PSDebug -Trace 0;
 
@@ -118,7 +118,7 @@ Write-Host "+DD+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #$labVmId = "/subscriptions/58c04a99-5b92-410c-9e41-10262f68ca80/resourceGroups/tSQLtCI_DevTestLab_3_RG/providers/Microsoft.DevTestLab/labs/tSQLtCI_DevTestLab_3/virtualmachines/SQL2014SP3D"
 
 
-Write-Host $GetUTCTimeStamp.Invoke()"Getting VM Resource Parameters";
+Write-Verbose $GetUTCTimeStamp.Invoke()"Getting VM Resource Parameters";
 
 ##(Get-AzResource -ResourceId (Get-AzResource -Name V1087sql2014sp3 -ResourceType Microsoft.DevTestLab/labs/virtualmachines -ResourceGroupName tSQLtCI_DevTestLab_20200323_1087_RG).ResourceId)
 $DTLVm = (Get-AzResource -Name $DTLVmName -ResourceType Microsoft.DevTestLab/labs/virtualmachines -ResourceGroupName $DTLRGName);
@@ -126,56 +126,56 @@ $DTLVmWithProperties = (Get-AzResource -ResourceId $DTLVm.ResourceId);
 
 
 $DTLVmWithProperties;
-Write-Host "<->4<-><-><-><-><-><-><-><-><-><-><-><-><->";
+Write-Verbose "<->4<-><-><-><-><-><-><-><-><-><-><-><-><->";
 
 $DTLVmComputeId = $DTLVmWithProperties.Properties.ComputeId
 $HiddenVmResourceId = $DTLVmComputeId;
-Write-Host "setting variable: DTLVmComputeId:" $DTLVmComputeId
+Write-Verbose "setting variable: DTLVmComputeId:" $DTLVmComputeId
 
 $HiddenVm = (Get-AzResource -Id $HiddenVmResourceId);
 $HiddenVmRGName = $HiddenVm.ResourceGroupName
-Write-Host "setting variable: HiddenVmRGName:" $HiddenVmRGName
+Write-Verbose "setting variable: HiddenVmRGName:" $HiddenVmRGName
 
 $HiddenVmName = $DTLVmWithProperties.Name
-Write-Host "setting variable: HiddenVmName:" $HiddenVmName
+Write-Verbose "setting variable: HiddenVmName:" $HiddenVmName
 
 $labVMId = $DTLVmWithProperties.ResourceId
-Write-Host 'labVMId: ' $labVMId
+Write-Verbose 'labVMId: ' $labVMId
 
 $HiddenVmPublicIpAddress= (Get-AzPublicIpAddress -ResourceGroupName $HiddenVmRGName -Name $HiddenVmName) ##Is this making use of an undocumented convention?
 $HiddenVmFQDN = $HiddenVmPublicIpAddress.DnsSettings.Fqdn
-Write-Host "setting variable: HiddenVmFQDN:" $HiddenVmFQDN
+Write-Verbose "setting variable: HiddenVmFQDN:" $HiddenVmFQDN
 
-Write-Host $GetUTCTimeStamp.Invoke()"Adding more Tags on ResourceGroup"
+Write-Verbose $GetUTCTimeStamp.Invoke()"Adding more Tags on ResourceGroup"
 
 $AddTagsToResourceGroup.Invoke($DTLRGName,@{"SQLVmFQDN"="$HiddenVmFQDN";"SQLVmPort"="$SQLPort";"SQLVersionEdition"="$SQLVersionEdition";"SQLVersion"="$SQLVersion";});
 
-Write-Host $GetUTCTimeStamp.Invoke()"Done: Adding more Tags on ResourceGroup"
-Write-Host $GetUTCTimeStamp.Invoke()'Starting the New VM'
+Write-Verbose $GetUTCTimeStamp.Invoke()"Done: Adding more Tags on ResourceGroup"
+Write-Verbose $GetUTCTimeStamp.Invoke()'Starting the New VM'
 
 ##Set-PSDebug -Trace 1;
 Start-AzVM -Name "$HiddenVmName" -ResourceGroupName "$HiddenVmRGName"
 Set-PSDebug -Trace 0;
 
-Write-Host $GetUTCTimeStamp.Invoke()'Done: Starting the New VM'
-Write-Host $GetUTCTimeStamp.Invoke()'Applying SqlVM Stuff'
+Write-Verbose $GetUTCTimeStamp.Invoke()'Done: Starting the New VM'
+Write-Verbose $GetUTCTimeStamp.Invoke()'Applying SqlVM Stuff'
 
 ##Set-PSDebug -Trace 1;
 $VM = New-AzResourceGroupDeployment -ResourceGroupName "$HiddenVmRGName" -TemplateFile "$dir\CreateSQLVirtualMachineTemplate.json" -sqlPortNumber "$SQLPort" -sqlAuthenticationLogin "$SQLUserName" -sqlAuthenticationPassword "$SQLPassword" -newVMName "$HiddenVmName" -newVMRID "$DTLVmComputeId"
 Set-PSDebug -Trace 0;
 
-Write-Host $GetUTCTimeStamp.Invoke()'Done: Applying SqlVM Stuff'
-Write-Host $GetUTCTimeStamp.Invoke()'Prep SQL Server for tSQLt Build'
+Write-Verbose $GetUTCTimeStamp.Invoke()'Done: Applying SqlVM Stuff'
+Write-Verbose $GetUTCTimeStamp.Invoke()'Prep SQL Server for tSQLt Build'
 
 $DS = Invoke-Sqlcmd -InputFile "$dir\PrepSQLServer.sql" -ServerInstance "$HiddenVmFQDN,$SQLPort" -Username "$SQLUserName" -Password "$SQLPassword"
 
 $DS = Invoke-Sqlcmd -InputFile "$dir\GetSQLServerVersion.sql" -ServerInstance "$HiddenVmFQDN,$SQLPort" -Username "$SQLUserName" -Password "$SQLPassword" -As DataSet
-$DS.Tables[0].Rows | %{ Write-Host "{ $($_['LoginName']), $($_['TimeStamp']), $($_['VersionDetail']), $($_['ProductVersion']), $($_['ProductLevel']), $($_['SqlVersion']) }" }
+$DS.Tables[0].Rows | %{ Write-Verbose "{ $($_['LoginName']), $($_['TimeStamp']), $($_['VersionDetail']), $($_['ProductVersion']), $($_['ProductLevel']), $($_['SqlVersion']) }" }
 
 $ActualSQLVersion = $DS.Tables[0].Rows[0]['SqlVersion'];
-Write-Host $ActualSQLVersion;
+Write-Verbose $ActualSQLVersion;
 
-Write-Host $GetUTCTimeStamp.Invoke()'Done: Prep SQL Server for tSQLt Build';
+Write-Verbose $GetUTCTimeStamp.Invoke()'Done: Prep SQL Server for tSQLt Build';
 
 
 Return @{
