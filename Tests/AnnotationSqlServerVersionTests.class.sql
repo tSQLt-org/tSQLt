8b726984b1e917de7b2eeb7f05bee6d3da14418b
mbt1 <bitbucket@meine.us>
2020-08-29 18:28:04 -0400

    jk - one more tests

--- Tests/AnnotationSqlServerVersionTests.class.sql
+++ Tests/AnnotationSqlServerVersionTests.class.sql
@@ -71,15 +71,24 @@ CREATE PROCEDURE MyInnerTests.[test should not execute] AS RAISERROR(''test exec
        @ExpectedMessage = 'test executed%';
 END;
 GO
+CREATE PROCEDURE AnnotationSqlServerVersionTests.[test provides a useful skip reason]
+AS
+BEGIN
+  EXEC tSQLt.NewTestClass 'MyInnerTests'
+  EXEC('
+--[@'+'tSQLt:MinSqlMajorVersion](43)
+CREATE PROCEDURE MyInnerTests.[test should not execute] AS RAISERROR(''test executed'',16,10);
+  ');
 
--- we need the MinSqlServerVersion annotation
--- we need a test that runs on all versions testing conclusively without using the logic it is testing.
--- put version info into tSQLt.info
--- write three tests 
--- add readableSqlVersion to tSQLt.info and write tests for it
--- write two pass-through tests for tSQLt.Private_SqlVersion
--- is there a CLR library that can get us the readable Sql Version?
--- require major.minor
--- reject %.%.%
+  EXEC tSQLt.FakeFunction @FunctionName = 'tSQLt.Private_SqlVersion', @FakeFunctionName = 'AnnotationSqlServerVersionTests.[42.17.1986.57]';
+
+  
+  EXEC tSQLt_testutil.AssertTestSkipped
+       @TestName = 'MyInnerTests.[test should not execute]',
+       @ExpectedMessage = 'asdasd'
+END;
+GO
+
+-- message
 
 SELECT CASE WHEN 'a' LIKE '%[^0-9.]%' THEN 1 ELSE 0 END
