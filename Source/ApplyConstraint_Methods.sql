c95969fc88b77b0b9fe2250f10679a200c94a4ab
nextaxtion <prithvicontact@gmail.com>
2020-10-02 13:27:28 +0530

    Update ApplyConstraint_Methods.sql
    Some formatting changes.

--- Source/ApplyConstraint_Methods.sql
+++ Source/ApplyConstraint_Methods.sql
@@ -40,9 +40,9 @@ RETURN
   SELECT TOP(1) constraints.object_id AS ConstraintObjectId, type_desc AS ConstraintType
     FROM sys.objects constraints
     CROSS JOIN tSQLt.Private_GetOriginalTableInfo(@TableObjectId) orgTbl
-   WHERE @ConstraintName IN (constraints.name, QUOTENAME(constraints.name))
-     AND constraints.parent_object_id = orgTbl.OrgTableObjectId
-   ORDER BY LEN(constraints.name) ASC;
+  WHERE @ConstraintName IN (constraints.name, QUOTENAME(constraints.name))
+    AND constraints.parent_object_id = orgTbl.OrgTableObjectId
+  ORDER BY LEN(constraints.name) ASC;
 GO
 
 CREATE FUNCTION tSQLt.Private_ResolveApplyConstraintParameters
