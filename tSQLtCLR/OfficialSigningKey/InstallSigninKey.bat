adb19035b20c3da0a33a33cf9cd46fa0409f1857
mbt1 <bitbucket@meine.us>
2020-10-17 13:22:25 -0400

    improved installation routine for new signing keys

--- tSQLtCLR/OfficialSigningKey/InstallSigninKey.bat
+++
@@ -1,7 +0,0 @@
-REM This installs the signing key for tSQLt on the local machine. 
-REM If you do not have access to the original key, create your own and use SN.exe to install it into the tSQLt_OfficialSigningKey container.
-REM This is only needed if you want to build tSQLt yourself.
-REM
-cd /d "%~dp0"
-"c:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64\sn.exe" -d tSQLt_OfficialSigningKey
-"c:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64\sn.exe" -i tSQLtOfficialSigningKey.pfx tSQLt_OfficialSigningKey
