03e79a1b796b756e7b35fd829ed8d69a656a94e6
Dmitrij Kultasev <dkultasev@gmail.com>
2020-10-23 10:59:04 +0300

    Fix issue with the long function names

--- Source/tSQLt.Private_PrepareFakeFunctionOutputTable.ssp.sql
+++ Source/tSQLt.Private_PrepareFakeFunctionOutputTable.ssp.sql
@@ -5,22 +5,22 @@ GO
 CREATE PROCEDURE tSQLt.Private_PrepareFakeFunctionOutputTable
     @FakeDataSource NVARCHAR(MAX),
     @FunctionName    NVARCHAR(MAX),
-    @OutputTable    sysname OUTPUT
+    @OutputTable    NVARCHAR(MAX) OUTPUT
 AS
     BEGIN
-        DECLARE @SchemaName SYSNAME;
+        DECLARE @SchemaName NVARCHAR(MAX);
         SET @SchemaName = COALESCE(PARSENAME(@FunctionName, 2), 'tSQlt');
         SET @FunctionName = LEFT(PARSENAME(@FunctionName, 1), 100);
-        SET @OutputTable = CONCAT(QUOTENAME(@SchemaName), '.', QUOTENAME(CONCAT(@FunctionName, '_', 'FakeFunctionOutputTable')));
+        SET @OutputTable = CONCAT(QUOTENAME(@SchemaName), '.', QUOTENAME(CONCAT(@FunctionName, '_FakeFunctionOutputTable')));
 
         IF ( LOWER(LTRIM(@FakeDataSource)) LIKE 'select%'
              AND OBJECT_ID(@FakeDataSource) IS NULL
            )
             BEGIN
-                SET @FakeDataSource = N'(' + @FakeDataSource + N') a';
+                SET @FakeDataSource = CONCAT(N'(', @FakeDataSource, N') a');
             END;
 
-        DECLARE @Cmd NVARCHAR(2000) = CONCAT('SELECT * INTO ', @OutputTable, ' FROM ', @FakeDataSource);
+        DECLARE @Cmd NVARCHAR(MAX) = CONCAT('SELECT * INTO ', @OutputTable, ' FROM ', @FakeDataSource);
 
         EXEC sp_executesql @Cmd;
 
