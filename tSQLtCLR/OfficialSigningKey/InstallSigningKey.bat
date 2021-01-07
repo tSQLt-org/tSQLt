f9b9e1833ddaf9f4526215828171b8b91e62dc1f
Liz Baron <10554+lizbaron@users.noreply.github.com>
2021-01-07 07:34:29 -0500

    master is broken anyway? So let's break it more.

--- tSQLtCLR/OfficialSigningKey/InstallSigningKey.bat
+++ tSQLtCLR/OfficialSigningKey/InstallSigningKey.bat
@@ -8,24 +8,39 @@ cd /d "%~dp0"
 DEL KeyInfo.txt
 DEL KeyInfoC.txt
 ECHO --------------------------------------------------------------------------------------------------------------------
+ECHO Are key containers computer-specific, or user-specific
+ECHO --------------------------------------------------------------------------------------------------------------------
+ECHO.
+"c:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64\sn.exe" -m
+ECHO.
+ECHO --------------------------------------------------------------------------------------------------------------------
 ECHO Deleting existing container (might fail)
 ECHO --------------------------------------------------------------------------------------------------------------------
+ECHO.
 "c:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64\sn.exe" -d tSQLt_OfficialSigningKey
+ECHO.
 ECHO --------------------------------------------------------------------------------------------------------------------
 ECHO Installing Key
 ECHO --------------------------------------------------------------------------------------------------------------------
+ECHO.
 "c:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64\sn.exe" -i tSQLtOfficialSigningKey.pfx tSQLt_OfficialSigningKey
+ECHO.
 ECHO --------------------------------------------------------------------------------------------------------------------
 ECHO Extracting Key Info
 ECHO --------------------------------------------------------------------------------------------------------------------
+ECHO.
 "c:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64\sn.exe" -p tSQLtOfficialSigningKey.pfx KeyInfo.txt sha1
 "c:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64\sn.exe" -pc tSQLt_OfficialSigningKey KeyInfoC.txt sha1
+ECHO.
 ECHO --------------------------------------------------------------------------------------------------------------------
 ECHO KeyInfo from file
 ECHO --------------------------------------------------------------------------------------------------------------------
+ECHO.
 "c:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64\sn.exe" -tp KeyInfo.txt 
+ECHO.
 ECHO --------------------------------------------------------------------------------------------------------------------
 ECHO KeyInfo from container
 ECHO --------------------------------------------------------------------------------------------------------------------
+ECHO.
 "c:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64\sn.exe" -tp KeyInfoC.txt 
 
