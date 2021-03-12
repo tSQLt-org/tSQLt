da3f681e1dcb08af5f64ed05b44f16f7e4c0b0ea
mbt1 <bitbucket@meine.us>
2021-03-12 10:49:44 -0500

    more experimentation code

--- Experiments/WAITFOR_DELAY_Tests.sql
+++ Experiments/WAITFOR_DELAY_Tests.sql
@@ -30,3 +30,42 @@ GO
 SELECT [@a],[@c],[@b],DATEDIFF(MICROSECOND,[@a],[@b])[@b-@a],DATEDIFF(MICROSECOND,[@a],[@c])[@c-@a],DATEDIFF(MICROSECOND,[@c],[@b])[@b-@c]
   FROM dbo.timelog AS T;
 GO
+--------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------
+GO
+SELECT *
+  FROM
+  (
+    SELECT [@a],[@c],[@b],DATEDIFF(MILLISECOND,[@a],[@b])[@b-@a],DATEDIFF(MILLISECOND,[@a],[@c])[@c-@a],DATEDIFF(MILLISECOND,[@c],[@b])[@b-@c]
+      FROM dbo.timelog AS T WITH(NOLOCK)
+  )X
+ WHERE X.[@b-@a]<108000 OR X.[@b-@c]<>0
+
+
+ GO
+
+ SELECT 
+     X.[@b-@a],
+     X.[@c-@a],
+     X.[@b-@c],
+     COUNT(1)
+  FROM
+  (
+    SELECT [@a],[@c],[@b],DATEDIFF(MILLISECOND,[@a],[@b])[@b-@a],DATEDIFF(MILLISECOND,[@a],[@c])[@c-@a],DATEDIFF(MILLISECOND,[@c],[@b])[@b-@c]
+      FROM dbo.timelog AS T WITH(NOLOCK)
+  )X
+ GROUP BY GROUPING SETS ((X.[@b-@a], X.[@c-@a], X.[@b-@c]),())
+ ORDER BY X.[@b-@a], X.[@c-@a], X.[@b-@c]
+
+
+ GO
+ SELECT oms, COUNT(1) cc
+ FROM(
+ SELECT RIGHT(ax,1) oms
+ FROM(
+   SELECT CONVERT(NVARCHAR(MAX),CAST([@a] AS DATETIME),121) ax FROM dbo.timelog
+ )x
+ )ox 
+ GROUP BY ox.oms;
+
