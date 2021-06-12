e453daffb72c10f00309761741e6416f3cabe948
Liz Baron <10554+lizbaron@users.noreply.github.com>
2021-06-12 17:21:58 -0400

    Implemented testing the Facades and retrieving the test results; also learned more about powershell parameters

--- Build/CommonFunctionsAndMethods.ps1
+++ Build/CommonFunctionsAndMethods.ps1
@@ -10,3 +10,25 @@ $GetUTCTimeStamp = {param();(Get-Date).ToUniversalTime().ToString('[yyyy-MM-ddTH
 Function Log-Output{[cmdletbinding()]Param([parameter(ValueFromPipeline)]$I);Process{Write-Host ([string]::Concat($GetUTCTimeStamp.Invoke()[0],[string]::Concat(" $I")));};};
 
 Log-Output($GetUTCTimeStamp.Invoke(),"Done: Loading CommonFunctionsAndMethods");
+
+Function Exec-SqlFile
+{
+  [cmdletbinding()]
+  param(
+    [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()][string] $ServerName,
+    [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()][string] $Login,
+    [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()][string] $SqlCmdPath,
+    [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()][string] $FileName,
+    [Parameter(Mandatory=$false)][string] $DatabaseName = "",
+    [Parameter(Mandatory=$false)][string] $AdditionalParameters = ""
+  );
+
+  $DatabaseSelector = "";
+  if($DatabaseName -ne ""){
+    $DatabaseSelector = '-d "'+$DatabaseName+'"';
+  }
+
+  $CallSqlCmd = '&"'+$SqlCmdPath+'\sqlcmd.exe" -S "'+$ServerName+'" '+$Login+' -b -I -i "'+$FileName+'" '+$DatabaseSelector+' '+$AdditionalParameters+';';
+  $CallSqlCmd = $CallSqlCmd + ';if($LASTEXITCODE -ne 0){throw "error during execution of "+$FileName;}'
+  Invoke-Expression $CallSqlCmd -ErrorAction Stop;
+}
--- Build/validateFacade.ps1
+++ Build/validateFacade.ps1
@@ -1,5 +1,4 @@
 <#
-- execute the ExecuteFacadeTest.sql file
 - validate that the dacpac is correct by installing tSQLt on one database; installing the database on another database; get the list of names from sys.objects where the name is not Private%; assert that it is the same list
 -- makesure that the database principal exists, [tSQLt.TestClasses] (maybe)
 -- 
@@ -24,27 +23,16 @@ Get-ChildItem -Path "output/*" -Include $facadeFiles | Remove-Item;
 
 Expand-Archive -Path "./output/tSQLtFacade.zip" -DestinationPath "./output";
 
-# "&quot;${sqlcmd.path}&quot;\sqlcmd ${execute.sql.sqlconnect} -I -i &quot;${execute.sql.executeas}&quot; ${execute.sql.filename} -v NewDbName=${db.name} DbName=${execute.sql.database} ExecuteStatement=&quot;${execute.sql.statement}&quot; -V11" />
-
 Push-Location;
 Set-Location './output';
 
-$CallSqlCmd = '&"'+$SqlCmdPath+'\sqlcmd.exe" -S "'+$ServerName+'" '+$Login+' -b -I -i ExecuteFacadeTests.sql -v FacadeSourceDb="'+$DatabaseName+'_src" FacadeTargetDb="'+$DatabaseName+'_tgt";';
-#$CallSqlCmd = '&"'+$SqlCmdPath+'\sqlcmd.exe" -S "'+$ServerName+'" '+$Login+' -I -b -Q "RAISERROR 50001" -v FacadeSourceDb="'+$DatabaseName+'_src" FacadeTargetDb="'+$DatabaseName+'_tgt";';
-$CallSqlCmd;
-#$CallSqlCmd = $CallSqlCmd + ';if($LASTEXITCODE -ne 0){throw "error during execution";}'
-Invoke-Expression $CallSqlCmd -ErrorAction Stop;
-
-<#
-- don't use DB names in test names
-- SQLCMD with a severity 15 error is not reporting an error
-- does a RAISERROR prevent the next batch from being executed
-- 
-#>
-throw "stuff"
+$AdditionalParameters = '-v FacadeSourceDb="'+$DatabaseName+'_src" FacadeTargetDb="'+$DatabaseName+'_tgt"'
+Exec-SqlFile -ServerName $ServerName -Login $Login -SqlCmdPath $SqlCmdPath -FileName "ExecuteFacadeTests.sql" -AdditionalParameters $AdditionalParameters;
 
 Set-Location '..';
-$CallSqlCmd = '&"'+$SqlCmdPath+'\sqlcmd.exe" -S "'+$ServerName+'" '+$Login+' -I -i GetTestResults.sql -d "'+$DatabaseName+'_src" -o "output/TestResults_Facade.xml"';
-Invoke-Expression $CallSqlCmd;
+$SourceDatabaseName = $DatabaseName+"_src";
+Exec-SqlFile -ServerName $ServerName -Login $Login -SqlCmdPath $SqlCmdPath -FileName "GetTestResults.sql" -DatabaseName $SourceDatabaseName -AdditionalParameters '-o "output/TestResults_Facade.xml"';
 
 Pop-Location;
+
+throw "where are our dacpacs?? oh no! See line 2"
