1010a0decea77e615be554da133c8d12bbc79907
mbt1 <bitbucket@meine.us>
2021-06-18 12:02:03 -0400

    something's not working

--- Build/FacadeBuildDacpac.ps1
+++ Build/FacadeBuildDacpac.ps1
@@ -11,8 +11,11 @@ Execute on a target server the Facade scripts
 
 EXEC Facade.CreateAllFacadeObjects
 #>
+"GotHere";
+
 
 $ServerNameTrimmed = $ServerName.Trim();
+$LoginTrimmed = $Login.Trim();
 
 $scriptpath = $MyInvocation.MyCommand.Path;
 $dir = Split-Path $scriptpath;
@@ -33,12 +36,15 @@ Set-Location './output';
 
 $SourceDatabaseName = $DatabaseName+"_src";
 $AdditionalParameters = '-v FacadeSourceDb="'+$SourceDatabaseName+'" FacadeTargetDb="'+$DatabaseName+'_tgt"'
-Exec-SqlFileOrQuery -ServerName $ServerNameTrimmed -Login $Login -SqlCmdPath $SqlCmdPath -FileName "ExecuteFacadeScript.sql" -AdditionalParameters $AdditionalParameters;
 
+"GotHere";
+Exec-SqlFileOrQuery -ServerName $ServerNameTrimmed -Login "$LoginTrimmed" -SqlCmdPath $SqlCmdPath -FileName "ExecuteFacadeScript.sql" -AdditionalParameters $AdditionalParameters;
 $QueryString = "DECLARE @FriendlyVersion NVARCHAR(128) = (SELECT FriendlyVersion FROM tSQLt.FriendlySQLServerVersion(CAST(SERVERPROPERTY('ProductVersion') AS NVARCHAR(128)))); PRINT @FriendlyVersion;";
+"GotHere";
 
-$resultSet = Exec-SqlFileOrQuery -ServerName $ServerNameTrimmed -Login $Login -SqlCmdPath $SqlCmdPath -Query $QueryString -DatabaseName $SourceDatabaseName;
+$resultSet = Exec-SqlFileOrQuery -ServerName $ServerNameTrimmed -Login "$LoginTrimmed" -SqlCmdPath $SqlCmdPath -Query $QueryString -DatabaseName $SourceDatabaseName;
 Log-Output "Friendly SQL Server Version: $resultSet";
+"GotHere";
 
 <# When using Windows Authentication, you must use "Integrated Security=SSPI" in the SqlConnectionString. Else use "User ID=<username>;Password=<password>;" #>
 $FacadeFileName = "tSQLtFacade."+$resultSet.Trim()+".dacpac";
@@ -61,7 +67,16 @@ if ($serverAlias.GetValueNames() -contains $ServerNameTrimmed) {
 
 $DacpacApplicationName = "tSQLtFacade."+$resultSet.Trim();
 
-$AuthenticationString = $Login.trim() -replace '^((\s*([-]U\s+)(?<user>\w+)\s*)|(\s*([-]P\s+)(?<password>\S+)\s*))+$', 'User Id=${user};Password="${password}"'
+if ($LoginTrimmed -match '((.*[-]U)|(.*[-]P))+.*)'){
+    $AuthenticationString = $LoginTrimmed -replace '^((\s*([-]U\s+)(?<user>\w+)\s*)|(\s*([-]P\s+)(?<password>\S+)\s*))+$', 'User Id=${user};Password="${password}"'  
+}
+elseif ($LoginTrimmed -eq '-E'){
+    $AuthenticationString = "Integrated Security=SSPI;";
+}
+else{
+    throw $LoginTrimmed + " is not supported here."
+}
+$AuthenticationString;
 $SqlConnectionString = "Data Source="+$resolvedServerName+";"+$AuthenticationString+";Connect Timeout=60;Initial Catalog="+$DatabaseName+"_tgt";
 & "$SqlPackagePath\sqlpackage.exe" /a:Extract /scs:"$SqlConnectionString" /tf:"$FacadeFileName" /p:DacApplicationName="$DacpacApplicationName" /p:IgnoreExtendedProperties=true /p:DacMajorVersion=0 /p:DacMinorVersion=1 /p:ExtractUsageProperties=false
 if($LASTEXITCODE -ne 0) {
