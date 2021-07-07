cae4dcae22f32e76a2a205b7ba35ee7721804f55
Liz Baron <10554+lizbaron@users.noreply.github.com>
2021-07-06 21:59:31 -0400

    Modified tSQLt step to create public files and validation files.

--- Build/BuildtSQLtZip.ps1
+++ Build/BuildtSQLtZip.ps1
@@ -6,13 +6,13 @@ $dir = Split-Path $scriptpath;
 
 $OutputPath = $dir + "/output/tSQLt/";
 $TempPath = $dir + "/temp/tSQLt/";
-$PublicOutputPath = OutputPath + "public/";
+$PublicOutputPath = $OutputPath + "public/";
 $PublicTempPath = $TempPath + "public/";
 $ValidationOutputPath = $OutputPath + "validation/";
 $FacadePath = $PublicTempPath + "/Facade/";
 $tSQLtFilesZipPath = $dir + "/output/tSQLtBuild/tSQLtFiles.zip";
-$PublicOutputFiles = @($dir + "/output/tSQLtBuild/ReadMe.txt", $dir + "/output/tSQLtBuild/tSQLtSnippets(SQLPrompt).zip");
-$ValidationOutputFiles = @($dir + "/output/tSQLtBuild/Version.txt", $dir + "/output/tSQLtBuild/CommitId.txt", $dir + "output/tSQLtBuild/tSQLt.tests.zip"); 
+$PublicOutputFiles = @(($dir + "/output/tSQLtBuild/ReadMe.txt"), ($dir + "/output/tSQLtBuild/tSQLtSnippets(SQLPrompt).zip"));
+$ValidationOutputFiles = @(($dir + "/output/tSQLtBuild/Version.txt"), ($dir + "/output/tSQLtBuild/CommitId.txt"), ($dir + "/output/tSQLtBuild/tSQLt.tests.zip")); 
 
 <# Clean #>
 Remove-DirectoryQuietly -Path $TempPath;
@@ -27,11 +27,17 @@ $validationOutputDir = New-Item -ItemType "directory" -Path $ValidationOutputPat
 Expand-Archive -Path ($tSQLtFilesZipPath) -DestinationPath $PublicTempPath;
 Get-ChildItem -Path ($dir + "/output/DacpacBuild/tSQLtFacade.*.dacpac") | Copy-Item -Destination $FacadePath;
 
+<# Create the tSQLt.zip in the public output path #>
 $compress = @{
     CompressionLevel = "Optimal"
     DestinationPath = $PublicOutputPath + "/tSQLt.zip"
 }
 Get-ChildItem -Path $PublicTempPath | Compress-Archive @compress
 
-<# ------------------------------------------------------------------------------ #>
+<# Copy all public files into public output path #>
+Copy-Item -Path $PublicOutputFiles -Destination $PublicOutputPath
+
+<# Copy all validation files into validation output path #>
+Copy-Item -Path $ValidationOutputFiles -Destination $ValidationOutputPath
+
 
