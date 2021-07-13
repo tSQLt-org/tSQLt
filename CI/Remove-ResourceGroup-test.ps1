32d9c083a1285a4d1b845f7d07ca2d9ce864facd
Liz Baron <10554+lizbaron@users.noreply.github.com>
2021-07-12 23:02:20 -0400

    Wild changes to which files are being used. Must test AZ_NightlyCleanup.yml before merging to main.

--- CI/Remove-ResourceGroup-test.ps1
+++
@@ -1,5 +0,0 @@
-﻿$GetUTCTimeStamp = {param();(Get-Date).ToUniversalTime().ToString('[yyyy-MM-ddTHH:mm:ss.fffffff UTC]');};
-
-Function Log-Output{[cmdletbinding()]Param([parameter(ValueFromPipeline)]$I);Process{Write-Host ([string]::Concat($GetUTCTimeStamp.Invoke()[0],[string]::Concat($I)));};};
-
-Log-Output "This is a test.";
