f7cfcf7cd565bdc6ec22addd757b95e857034e99
Liz Baron <10554+lizbaron@users.noreply.github.com>
2021-07-12 21:46:15 -0400

    isolate imported file

--- CI/Remove-ResourceGroup-test.ps1
+++ CI/Remove-ResourceGroup-test.ps1
@@ -1,1 +1,5 @@
-﻿  Write-Output "▀-▄-_-▄-▀-▄-_-▄-▀-▄-_-▄-▀-▄-_-▄-▀-▄-_-▄-▀-▄-_-▄-▀-▄-_-▄-▀-▄-_-▄-▀-▄-_-▄-▀-▄-_-▄-▀-▄-_-▄-";
+﻿$GetUTCTimeStamp = {param();(Get-Date).ToUniversalTime().ToString('[yyyy-MM-ddTHH:mm:ss.fffffff UTC]');};
+
+Function Log-Output{[cmdletbinding()]Param([parameter(ValueFromPipeline)]$I);Process{Write-Host ([string]::Concat($GetUTCTimeStamp.Invoke()[0],[string]::Concat($I)));};};
+
+Log-Output "This is a test.";
