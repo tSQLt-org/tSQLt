b7bd8ce577421b7fe77a165eca9a9285f9011619
Liz Baron <10554+lizbaron@users.noreply.github.com>
2021-09-01 22:24:19 -0400

    Do the slashes really ahve to be consistent?

--- Build/FacadeValidate.ps1
+++ Build/FacadeValidate.ps1
@@ -18,19 +18,19 @@ $dir = Split-Path $scriptpath
 
 Log-Output "FileLocation: $dir"
 
-$TempPath = $dir + "/temp/Validate";
-$FacadeFilesPath = $TempPath + "/Facade";
-$TestResultsPath = $TempPath + "/TestResults/TestResults_Facade.xml";
+$TempPath = $dir + "\temp\Validate";
+$FacadeFilesPath = $TempPath + "\Facade";
+$TestResultsPath = $TempPath + "\TestResults|TestResults_Facade.xml";
 
 $ServerNameTrimmed = $ServerName.Trim();
 $LoginTrimmed = $Login.Trim("'").Trim();
 
-Expand-Archive -Path "./output/tSQLt/validation/tSQLtFacade.zip" -DestinationPath $FacadeFilesPath;
+Expand-Archive -Path ".\output\tSQLt\validation\tSQLtFacade.zip" -DestinationPath $FacadeFilesPath;
 
 Push-Location;
 
 $FriendlySQLServerVersion = Get-FriendlySQLServerVersion -ServerName $ServerNameTrimmed -Login "$LoginTrimmed" -SqlCmdPath $SqlCmdPath -DatabaseName $DatabaseName;
-$FacadeFileName = $TempPath + "/tSQLt/Facade/tSQLtFacade."+$FriendlySQLServerVersion+".dacpac";
+$FacadeFileName = $TempPath + "\tSQLt\Facade\tSQLtFacade."+$FriendlySQLServerVersion+".dacpac";
 
 $DacpacDatabaseName = $DatabaseName+"_dacpac";
 $AdditionalParameters = '-v NewDbName="'+$DacpacDatabaseName+'"';
