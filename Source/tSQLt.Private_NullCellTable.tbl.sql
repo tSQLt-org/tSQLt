196b4d0b4df61f6c9ab091af7b7ebec37712bfab
Liz Baron <10554+lizbaron@users.noreply.github.com>
2021-09-24 06:55:46 -0400

    Black magic --> tSQLt.Private_NullCellTable will contain a NULL row always, except when it doesn't, and then if you do anything to it, it will again.

--- Source/tSQLt.Private_NullCellTable.tbl.sql
+++ Source/tSQLt.Private_NullCellTable.tbl.sql
@@ -1,18 +1,26 @@
+IF OBJECT_ID('tSQLt.Private_NullCellTable_PreventTruncate') IS NOT NULL DROP TABLE tSQLt.Private_NullCellTable_PreventTruncate;
 IF OBJECT_ID('tSQLt.Private_NullCellTable') IS NOT NULL DROP TABLE tSQLt.Private_NullCellTable;
 GO
 ---Build+
 GO
 CREATE TABLE tSQLt.Private_NullCellTable(
-  I INT 
+  I INT CONSTRAINT[U:tSQLt.Private_NullCellTable] UNIQUE CLUSTERED
+);
+GO
+
+CREATE TABLE tSQLt.Private_NullCellTable_PreventTruncate (
+  I INT CONSTRAINT[FK:tSQLt.Private_NullCellTable(I)] FOREIGN KEY REFERENCES tSQLt.Private_NullCellTable(I)
 );
 GO
 
 INSERT INTO tSQLt.Private_NullCellTable (I) VALUES (NULL);
 GO
 
-CREATE TRIGGER tSQLt.Private_NullCellTable_StopDeletes ON tSQLt.Private_NullCellTable INSTEAD OF DELETE, INSERT, UPDATE
+CREATE TRIGGER tSQLt.Private_NullCellTable_StopModifications ON tSQLt.Private_NullCellTable INSTEAD OF DELETE, INSERT, UPDATE
 AS
 BEGIN
-  RETURN;
+  IF EXISTS (SELECT 1 FROM tSQLt.Private_NullCellTable) RETURN;
+  INSERT INTO tSQLt.Private_NullCellTable VALUES (NULL);
 END;
 GO
+
