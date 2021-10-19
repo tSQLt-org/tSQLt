fdd7bf1e20498a1e276b4b18ab4bda37a131f488
Liz Baron <10554+lizbaron@users.noreply.github.com>
2021-10-18 23:27:26 -0400

    starting UndoTestDoublesTests.class.sql and related tests

---
+++ Source/tSQLt.UndoTestDoubles.ssp.sql
@@ -0,0 +1,12 @@
+IF OBJECT_ID('tSQLt.UndoTestDoubles') IS NOT NULL DROP PROCEDURE tSQLt.UndoTestDoubles;
+GO
+---Build+
+GO
+CREATE PROCEDURE tSQLt.UndoTestDoubles
+AS
+BEGIN
+  RETURN;
+END;
+GO
+
+
---
+++ Tests/UndoTestDoublesTests.class.sql
@@ -0,0 +1,22 @@
+EXEC tSQLt.NewTestClass 'UndoTestDoublesTests';
+GO
+CREATE PROCEDURE UndoTestDoublesTests.[test restores a faked table]
+AS
+BEGIN
+  CREATE TABLE UndoTestDoublesTests.aSimpleTable 
+  (
+    Id INT
+  );
+
+  DECLARE @OriginalObjectId INT = OBJECT_ID('UndoTestDoublesTests.aSimpleTable');
+
+  EXEC tSQLt.FakeTable @TableName = 'UndoTestDoublesTests.aSimpleTable';
+
+  EXEC tSQLt.UndoTestDoubles;
+
+  DECLARE @RestoredObjectId INT = OBJECT_ID('UndoTestDoublesTests.aSimpleTable');
+  EXEC tSQLt.AssertEquals @Expected = @OriginalObjectId, @Actual = @RestoredObjectId;
+
+END;
+GO
+
