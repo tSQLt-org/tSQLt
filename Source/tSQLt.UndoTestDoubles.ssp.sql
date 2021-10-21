f4c4c91876c359a9c857c64316bea0c928506926
Liz Baron <10554+lizbaron@users.noreply.github.com>
2021-10-20 23:01:27 -0400

    wrote a test which passes. ignored a test which fails. more work required. also BuildOrder.txt is yet again... important.

--- Source/tSQLt.UndoTestDoubles.ssp.sql
+++ Source/tSQLt.UndoTestDoubles.ssp.sql
@@ -5,6 +5,12 @@ GO
 CREATE PROCEDURE tSQLt.UndoTestDoubles
 AS
 BEGIN
+  SELECT * FROM tSQLt.Private_RenamedObjectLog
+  SELECT 
+      QUOTENAME(SCHEMA_NAME(T.schema_id))+'.'+QUOTENAME(T.name) OriginalName, 
+      OBJECT_SCHEMA_NAME(OTI.OrgTableObjectId),
+      OBJECT_NAME(OTI.OrgTableObjectId)
+    FROM sys.tables T CROSS APPLY tSQLt.Private_GetOriginalTableInfo(T.object_id) OTI
   RETURN;
 END;
 GO
--- Tests/UndoTestDoublesTests.class.sql
+++ Tests/UndoTestDoublesTests.class.sql
@@ -1,5 +1,15 @@
 EXEC tSQLt.NewTestClass 'UndoTestDoublesTests';
 GO
+CREATE PROCEDURE UndoTestDoublesTests.[test doesn't fail if there's no test double in the database]
+AS
+BEGIN
+
+  EXEC tSQLt.ExpectNoException;
+  
+  EXEC tSQLt.UndoTestDoubles;
+
+END;
+GO
 CREATE PROCEDURE UndoTestDoublesTests.[test restores a faked table]
 AS
 BEGIN
