06fc8dc5a5d759876aa678c3e8d2048b69facdac
Liz Baron <10554+lizbaron@users.noreply.github.com>
2021-10-21 22:43:26 -0400

    some refactoring...

--- Source/tSQLt.UndoTestDoubles.ssp.sql
+++ Source/tSQLt.UndoTestDoubles.ssp.sql
@@ -5,12 +5,13 @@ GO
 CREATE PROCEDURE tSQLt.UndoTestDoubles
 AS
 BEGIN
-  SELECT * FROM tSQLt.Private_RenamedObjectLog
   SELECT 
-      QUOTENAME(SCHEMA_NAME(T.schema_id))+'.'+QUOTENAME(T.name) OriginalName, 
-      OBJECT_SCHEMA_NAME(OTI.OrgTableObjectId),
-      OBJECT_NAME(OTI.OrgTableObjectId)
-    FROM sys.tables T CROSS APPLY tSQLt.Private_GetOriginalTableInfo(T.object_id) OTI
+      Id,
+      ObjectId,
+      OBJECT_SCHEMA_NAME(L.ObjectId) SchemaName,
+      OBJECT_NAME(L.ObjectId) CurrentName,
+      OriginalName 
+    FROM tSQLt.Private_RenamedObjectLog L
   RETURN;
 END;
 GO
