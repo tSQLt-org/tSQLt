a7afa7c566d82ac1753aa57fec40b98b10c0d3ae
mbt1 <bitbucket@meine.us>
2021-10-27 00:29:09 -0400

    added a parsing challenge

---
+++ Experiments/ParsingDisaster.sql
@@ -0,0 +1,17 @@
+SELECT 1 AS [A /*
+GO
+*/ O]
+
+--/* <-- execute, then delete '--' and execute again
+/*Comment*/
+SELECT 2
+,2.2
+--*/SELECT 3    /*
+--*/,3.2,'
+SELECT 4
+,4.2
+--'             /*
+--*/SELECT 5    /*
+--*/,5.2        /*
+--*/ 
+SELECT 6
