99794687de821e3c94f4228d6c571724049ebc2f
Liz Baron <10554+lizbaron@users.noreply.github.com>
2021-10-27 23:05:48 -0400

    UndoTestDoubles is working. Check out the TODO in UndoTestDoublesTests.class.sql for next steps.

--- Source/tSQLt.UndoTestDoubles.ssp.sql
+++ Source/tSQLt.UndoTestDoubles.ssp.sql
@@ -5,14 +5,35 @@ GO
 CREATE PROCEDURE tSQLt.UndoTestDoubles
 AS
 BEGIN
-  SELECT 
-      Id,
-      ObjectId,
-      OBJECT_SCHEMA_NAME(L.ObjectId) SchemaName,
-      OBJECT_NAME(L.ObjectId) CurrentName,
-      OriginalName 
-    FROM tSQLt.Private_RenamedObjectLog L
-  RETURN;
+  DECLARE @cmd NVARCHAR(MAX);
+  WITH L AS
+  (
+    SELECT 
+        ROL.Id,
+        ParentROL.Id ParentId,
+        ISNULL(ParentROL.Id,ROL.Id) SortId,
+        ROL.ObjectId,
+        OBJECT_SCHEMA_NAME(ROL.ObjectId) SchemaName,
+        OBJECT_NAME(ROL.ObjectId) CurrentName,
+        PARSENAME(ROL.OriginalName,1) OriginalName,
+        O.type ObjectType
+      FROM tSQLt.Private_RenamedObjectLog ROL
+      JOIN sys.objects O
+        ON ROL.ObjectId = O.object_id
+      LEFT JOIN tSQLt.Private_RenamedObjectLog ParentROL
+        ON O.parent_object_id = ParentROL.ObjectId
+  )
+  SELECT @cmd = 
+  (
+    SELECT 
+        CASE WHEN L.ParentId IS NULL THEN DC.cmd ELSE '' END+
+        ';EXEC tSQLt.Private_RenameObject '''+L.SchemaName+''','''+L.CurrentName+''','''+L.OriginalName+''';'
+      FROM L
+     CROSS APPLY tSQLt.Private_GetDropItemCmd(QUOTENAME(L.SchemaName)+'.'+QUOTENAME(L.OriginalName),L.ObjectType) DC
+     ORDER BY L.SortId DESC, L.Id ASC
+       FOR XML PATH(''),TYPE
+  ).value('.','NVARCHAR(MAX)')
+  EXEC(@cmd);
 END;
 GO
 
--- Tests/UndoTestDoublesTests.class.sql
+++ Tests/UndoTestDoublesTests.class.sql
@@ -13,10 +13,7 @@ GO
 CREATE PROCEDURE UndoTestDoublesTests.[test restores a faked table]
 AS
 BEGIN
-  CREATE TABLE UndoTestDoublesTests.aSimpleTable 
-  (
-    Id INT
-  );
+  CREATE TABLE UndoTestDoublesTests.aSimpleTable ( Id INT );
 
   DECLARE @OriginalObjectId INT = OBJECT_ID('UndoTestDoublesTests.aSimpleTable');
 
@@ -29,10 +26,121 @@ BEGIN
 
 END;
 GO
+CREATE PROCEDURE UndoTestDoublesTests.[test works with names in need of quotes]
+AS
+BEGIN
+  EXEC('CREATE SCHEMA [A Random Schema];');
+  CREATE TABLE [A Random Schema].[A Simple Table] 
+  (
+    Id INT
+  );
+
+  DECLARE @OriginalObjectId INT = OBJECT_ID('[A Random Schema].[A Simple Table]');
+
+  EXEC tSQLt.FakeTable @TableName = '[A Random Schema].[A Simple Table]';
+
+  EXEC tSQLt.UndoTestDoubles;
+
+  DECLARE @RestoredObjectId INT = OBJECT_ID('[A Random Schema].[A Simple Table]');
+  EXEC tSQLt.AssertEquals @Expected = @OriginalObjectId, @Actual = @RestoredObjectId;
+
+END;
+GO
+CREATE PROCEDURE UndoTestDoublesTests.[test restores many faked tables]
+AS
+BEGIN
+  CREATE TABLE UndoTestDoublesTests.aSimpleTable1 ( Id INT );
+  CREATE TABLE UndoTestDoublesTests.aSimpleTable2 ( Id INT );
+  CREATE TABLE UndoTestDoublesTests.aSimpleTable3 ( Id INT );
+
+  SELECT X.TableName, OBJECT_ID('UndoTestDoublesTests.'+X.TableName) ObjectId
+    INTO #OriginalObjectIds
+    FROM (VALUES('aSimpleTable1'),('aSimpleTable2'),('aSimpleTable3')) X (TableName);
+
+  EXEC tSQLt.FakeTable @TableName = 'UndoTestDoublesTests.aSimpleTable1';
+  EXEC tSQLt.FakeTable @TableName = 'UndoTestDoublesTests.aSimpleTable2';
+  EXEC tSQLt.FakeTable @TableName = 'UndoTestDoublesTests.aSimpleTable3';
+
+  EXEC tSQLt.UndoTestDoubles;
+
+  SELECT X.TableName, OBJECT_ID('UndoTestDoublesTests.'+X.TableName) ObjectId
+    INTO #RestoredObjectIds
+    FROM (VALUES('aSimpleTable1'),('aSimpleTable2'),('aSimpleTable3')) X (TableName);
+
+  EXEC tSQLt.AssertEqualsTable @Expected = '#OriginalObjectIds', @Actual = '#RestoredObjectIds';
+
+END;
+GO
+CREATE PROCEDURE UndoTestDoublesTests.[test restores a constraint]
+AS
+BEGIN
+  CREATE TABLE UndoTestDoublesTests.aSimpleTable ( Id INT CONSTRAINT aSimpleTableConstraint CHECK(Id > 0));
+
+  SELECT X.ObjectName, OBJECT_ID('UndoTestDoublesTests.'+X.ObjectName) ObjectId
+    INTO #OriginalObjectIds
+    FROM (VALUES('aSimpleTableConstraint')) X (ObjectName);
+
+  EXEC tSQLt.FakeTable @TableName = 'UndoTestDoublesTests.aSimpleTable';
+  EXEC tSQLt.ApplyConstraint @TableName = 'UndoTestDoublesTests.aSimpleTable', @ConstraintName = 'aSimpleTableConstraint';
+
+  EXEC tSQLt.UndoTestDoubles;
+
+  SELECT X.ObjectName, OBJECT_ID('UndoTestDoublesTests.'+X.ObjectName) ObjectId
+    INTO #RestoredObjectIds
+    FROM (VALUES('aSimpleTableConstraint')) X (ObjectName);
+
+  EXEC tSQLt.AssertEqualsTable @Expected = '#OriginalObjectIds', @Actual = '#RestoredObjectIds';
+
+END;
+GO
+CREATE PROCEDURE UndoTestDoublesTests.[test restores a table that has been faked multiple times]
+AS
+BEGIN
+  CREATE TABLE UndoTestDoublesTests.aSimpleTable ( Id INT );
+
+  DECLARE @OriginalObjectId INT = OBJECT_ID('UndoTestDoublesTests.aSimpleTable');
+
+  EXEC tSQLt.FakeTable @TableName = 'UndoTestDoublesTests.aSimpleTable';
+  EXEC tSQLt.FakeTable @TableName = 'UndoTestDoublesTests.aSimpleTable';
+  EXEC tSQLt.FakeTable @TableName = 'UndoTestDoublesTests.aSimpleTable';
+
+  EXEC tSQLt.UndoTestDoubles;
+
+  DECLARE @RestoredObjectId INT = OBJECT_ID('UndoTestDoublesTests.aSimpleTable');
+  EXEC tSQLt.AssertEquals @Expected = @OriginalObjectId, @Actual = @RestoredObjectId;
+
+END;
+GO
+CREATE PROCEDURE UndoTestDoublesTests.[test restores a trigger]
+AS
+BEGIN
+  CREATE TABLE UndoTestDoublesTests.aSimpleTable ( Id INT );
+  EXEC('CREATE TRIGGER aSimpleTrigger ON UndoTestDoublesTests.aSimpleTable FOR INSERT AS RETURN;');
+
+  SELECT X.ObjectName, OBJECT_ID('UndoTestDoublesTests.'+X.ObjectName) ObjectId
+    INTO #OriginalObjectIds
+    FROM (VALUES('aSimpleTrigger')) X (ObjectName);
+
+  EXEC tSQLt.FakeTable @TableName = 'UndoTestDoublesTests.aSimpleTable';
+  EXEC tSQLt.ApplyTrigger @TableName = 'UndoTestDoublesTests.aSimpleTable', @TriggerName = 'aSimpleTrigger';
+
+  EXEC tSQLt.UndoTestDoubles;
+
+  SELECT X.ObjectName, OBJECT_ID('UndoTestDoublesTests.'+X.ObjectName) ObjectId
+    INTO #RestoredObjectIds
+    FROM (VALUES('aSimpleTrigger')) X (ObjectName);
+
+  EXEC tSQLt.AssertEqualsTable @Expected = '#OriginalObjectIds', @Actual = '#RestoredObjectIds';
+
+END;
+GO
 /*--
 TODO
-- ApplyConstraint
-- ApplyTrigger
+==> multiple constraints and triggers on a multiple tables, faked multiple times
+- stored procedures
+- views
+- functions
+
 
 Also, just review all the code.
 
