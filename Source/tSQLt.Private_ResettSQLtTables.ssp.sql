66f8674967a98932396abc65c6de17283d40e24e
Liz Baron <10554+lizbaron@users.noreply.github.com>
2021-11-14 17:57:00 -0500

    Added Private_ResettSQLtTablesTests.class.sql, started writing tests for Private_CleanUp, we also need the ssmssqlproj files so that the files show up in SSMS (deprecated).

---
+++ Source/tSQLt.Private_ResettSQLtTables.ssp.sql
@@ -0,0 +1,10 @@
+IF OBJECT_ID('tSQLt.Private_ResettSQLtTables') IS NOT NULL DROP PROCEDURE tSQLt.Private_ResettSQLtTables;
+GO
+---Build+
+GO
+CREATE PROCEDURE tSQLt.Private_ResettSQLtTables
+AS
+BEGIN
+  RETURN;
+END;
+GO
