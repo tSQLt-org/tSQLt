abdd7f03e059c1a82cf24e68e5898e578889df56
Liz Baron <10554+lizbaron@users.noreply.github.com>
2021-11-17 12:56:19 -0500

    Finished writing tests for Reset/Restore, but we still need tests for Reset/Error condition, Ignore, Truncate, and Remove.

--- Source/tSQLt.Private_NoTransactionHandleTable.ssp.sql
+++ Source/tSQLt.Private_NoTransactionHandleTable.ssp.sql
@@ -8,36 +8,61 @@ CREATE PROCEDURE tSQLt.Private_NoTransactionHandleTable
 @TableAction NVARCHAR(MAX)
 AS
 BEGIN
-  IF (@Action = 'Save')
-  BEGIN
-    IF (@TableAction = 'Restore')
+  DECLARE @cmd NVARCHAR(MAX);
+  BEGIN TRY
+    IF (OBJECT_ID(@FullTableName) IS NULL) RAISERROR('Table %s does not exist.',16,10,@FullTableName);
+
+    IF (@Action = 'Save')
     BEGIN
-      DECLARE @NewQuotedNameForBackupTable NVARCHAR(MAX) = '[tSQLt].'+QUOTENAME(tSQLt.Private::CreateUniqueObjectName());
-      DECLARE @Cmd NVARCHAR(MAX) = 'SELECT * INTO '+@NewQuotedNameForBackupTable+' FROM '+@FullTableName+';';
-      EXEC (@Cmd);
-      INSERT INTO #TableBackupLog (OriginalName, BackupName) VALUES (@FullTableName, @NewQuotedNameForBackupTable);
-      EXEC tSQLt.Private_MarktSQLtTempObject @ObjectName = @NewQuotedNameForBackupTable, @ObjectType = N'TABLE', @NewNameOfOriginalObject = NULL; 
+      IF (@TableAction = 'Restore')
+      BEGIN
+        DECLARE @NewQuotedNameForBackupTable NVARCHAR(MAX) = '[tSQLt].'+QUOTENAME(tSQLt.Private::CreateUniqueObjectName());
+        SET @cmd = 'SELECT * INTO '+@NewQuotedNameForBackupTable+' FROM '+@FullTableName+';';
+        EXEC (@Cmd);
+        INSERT INTO #TableBackupLog (OriginalName, BackupName) VALUES (@FullTableName, @NewQuotedNameForBackupTable);
+        EXEC tSQLt.Private_MarktSQLtTempObject @ObjectName = @NewQuotedNameForBackupTable, @ObjectType = N'TABLE', @NewNameOfOriginalObject = NULL; 
+      END;
+      ELSE IF (@TableAction = 'Remove')
+      BEGIN
+        EXEC tSQLt.RemoveObject @ObjectName = @FullTableName;
+      END;
+      ELSE IF (@TableAction IN ('Truncate', 'Ignore'))
+      BEGIN
+        RETURN;
+      END;
+      ELSE
+      BEGIN
+        RAISERROR('Invalid @TableAction parameter value.',16,10);
+      END;
     END;
-    ELSE IF (@TableAction = 'Remove')
+    ELSE IF (@Action = 'Reset')
     BEGIN
-      EXEC tSQLt.RemoveObject @ObjectName = @FullTableName;
-    END;
-    ELSE IF (@TableAction IN ('Truncate', 'Ignore'))
-    BEGIN
-      RETURN;
+      IF (@TableAction = 'Restore')
+      BEGIN
+        DECLARE @BackupTableName NVARCHAR(MAX) =(SELECT BackupName FROM #TableBackupLog WHERE OriginalName = @FullTableName);
+        SET @cmd = 'DELETE FROM ' + @FullTableName + ';';
+        IF (EXISTS(SELECT 1 FROM sys.columns WHERE object_id = OBJECT_ID(@FullTableName) AND is_identity = 1))
+        BEGIN
+          SET @cmd = @cmd + 'SET IDENTITY_INSERT ' + @FullTableName + ' ON;';
+        END;
+        SET @cmd = @cmd + 'INSERT INTO ' + @FullTableName +'(';
+        SET @cmd = @cmd + STUFF((SELECT ','+QUOTENAME(name) FROM sys.columns WHERE object_id = OBJECT_ID(@FullTableName) ORDER BY column_id FOR XML PATH(''),TYPE).value('.','NVARCHAR(MAX)'),1,1,'');
+        SET @cmd = @cmd + ') SELECT * FROM ' + @BackupTableName+';';
+        EXEC(@cmd);
+      END
     END;
     ELSE
     BEGIN
-      RAISERROR('Invalid @TableAction parameter value. tSQLt is in an unknown state: Stopping execution.',16,10);
+      RAISERROR('Invalid @Action parameter value.',16,10);
     END;
-  END;
-  ELSE IF (@Action = 'Reset')
-  BEGIN
-    PRINT 'Reset!';
-  END;
-  ELSE
-  BEGIN
-    RAISERROR('Invalid @Action parameter value. tSQLt is in an unknown state: Stopping execution.',16,10);
-  END;
+  END TRY
+  BEGIN CATCH
+    DECLARE @ErrorLine INT = ERROR_LINE();
+    DECLARE @ErrorProcedure NVARCHAR(MAX) = ERROR_PROCEDURE();
+    DECLARE @ErrorMessage NVARCHAR(MAX) = ERROR_MESSAGE();
+    DECLARE @ErrorSeverity INT = ERROR_SEVERITY();
+    DECLARE @ErrorState INT = ERROR_STATE();
+    RAISERROR('tSQLt is in an unknown state: Stopping execution. (%s | Procedure: %s | Line: %i)', @ErrorSeverity, @ErrorState, @ErrorMessage, @ErrorProcedure, @ErrorLine);
+  END CATCH;
 END;
 GO
--- Tests/Private_NoTransactionHandleTableTests.class.sql
+++ Tests/Private_NoTransactionHandleTableTests.class.sql
@@ -1,13 +1,18 @@
 EXEC tSQLt.NewTestClass 'Private_NoTransactionHandleTableTests';
 GO
+/*-----------------------------------------------------------------------------------------------*/
+GO
 CREATE PROCEDURE Private_NoTransactionHandleTableTests.[test errors if Action is not an acceptable value]
 AS
 BEGIN
+  CREATE TABLE Private_NoTransactionHandleTableTests.Table1 (Id INT);
   
-  EXEC tSQLt.ExpectException @ExpectedMessage = 'Invalid @Action parameter value. tSQLt is in an unknown state: Stopping execution.', @ExpectedSeverity = 16, @ExpectedState = 10;
+  EXEC tSQLt.ExpectException @ExpectedMessagePattern = 'tSQLt is in an unknown state: Stopping execution. (Invalid @Action parameter value. | Procedure: tSQLt.Private_NoTransactionHandleTable | Line: %)', @ExpectedSeverity = 16, @ExpectedState = 10;
 
-  EXEC tSQLt.Private_NoTransactionHandleTable @Action = 'Unexpected Action', @FullTableName = '[someschema].[sometable]', @TableAction = 'Restore';
+  EXEC tSQLt.Private_NoTransactionHandleTable @Action = 'Unexpected Action', @FullTableName = 'Private_NoTransactionHandleTableTests.Table1', @TableAction = 'Restore';
 END;
+GO
+/*-----------------------------------------------------------------------------------------------*/
 GO
 CREATE PROCEDURE Private_NoTransactionHandleTableTests.[test creates new table and saves its name in #TableBackupLog if Action is Save]
 AS
@@ -22,6 +27,8 @@ BEGIN
 
   EXEC tSQLt.AssertEqualsTableSchema @Expected = 'Private_NoTransactionHandleTableTests.Table1', @Actual = @BackupName;
 END;
+GO
+/*-----------------------------------------------------------------------------------------------*/
 GO
 CREATE PROCEDURE Private_NoTransactionHandleTableTests.[test calls tSQLt.Private_MarktSQLtTempObject on new object]
 AS
@@ -45,6 +52,8 @@ BEGIN
 
   EXEC tSQLt.AssertEqualsTable '#Expected','#Actual';
 END;
+GO
+/*-----------------------------------------------------------------------------------------------*/
 GO
 CREATE PROCEDURE Private_NoTransactionHandleTableTests.[test does not create a backup table if @Action is Save and the @TableAction is Truncate]
 AS
@@ -74,6 +83,8 @@ BEGIN
   EXEC tSQLt.AssertEmptyTable @TableName = '#Actual';
 END;
 GO
+/*-----------------------------------------------------------------------------------------------*/
+GO
 CREATE PROCEDURE Private_NoTransactionHandleTableTests.[test does not create a backup table if @Action is Save and the @TableAction is Ignore]
 AS
 BEGIN
@@ -102,20 +113,25 @@ BEGIN
   EXEC tSQLt.AssertEmptyTable @TableName = '#Actual';
 END;
 GO
+/*-----------------------------------------------------------------------------------------------*/
+GO
 CREATE PROCEDURE Private_NoTransactionHandleTableTests.[test calls tSQLt.RemoveObject if @Action is Save and @TableAction is Remove]
 AS
 BEGIN
+  CREATE TABLE Private_NoTransactionHandleTableTests.Table1 (Id INT, col1 NVARCHAR(MAX));
   EXEC tSQLt.SpyProcedure @ProcedureName = 'tSQLt.RemoveObject';
 
-  EXEC tSQLt.Private_NoTransactionHandleTable @Action = 'Save', @FullTableName = 'Some.Table', @TableAction = 'Remove';
+  EXEC tSQLt.Private_NoTransactionHandleTable @Action = 'Save', @FullTableName = 'Private_NoTransactionHandleTableTests.Table1', @TableAction = 'Remove';
 
   SELECT ObjectName INTO #Actual FROM tSQLt.RemoveObject_SpyProcedureLog;
   SELECT TOP(0) A.* INTO #Expected FROM #Actual A RIGHT JOIN #Actual X ON 1=0;
   INSERT INTO #Expected
-  VALUES('Some.Table');
+  VALUES('Private_NoTransactionHandleTableTests.Table1');
   EXEC tSQLt.AssertEqualsTable '#Expected','#Actual';
   
 END;
+GO
+/*-----------------------------------------------------------------------------------------------*/
 GO
 CREATE PROCEDURE Private_NoTransactionHandleTableTests.[test does not create a backup table if @Action is Save and @TableAction is Remove]
 AS
@@ -146,37 +162,106 @@ BEGIN
   EXEC tSQLt.AssertEmptyTable @TableName = '#Actual';
 END;
 GO
+/*-----------------------------------------------------------------------------------------------*/
+GO
 CREATE PROCEDURE Private_NoTransactionHandleTableTests.[test errors if @Action is Save and @TableAction is not an acceptable value]
 AS
 BEGIN
+  CREATE TABLE Private_NoTransactionHandleTableTests.SomeTable(i INT);
   
-  EXEC tSQLt.ExpectException @ExpectedMessage = 'Invalid @TableAction parameter value. tSQLt is in an unknown state: Stopping execution.', @ExpectedSeverity = 16, @ExpectedState = 10;
+  EXEC tSQLt.ExpectException @ExpectedMessagePattern = 'tSQLt is in an unknown state: Stopping execution. (Invalid @TableAction parameter value. | Procedure: tSQLt.Private_NoTransactionHandleTable | Line: %)', @ExpectedSeverity = 16, @ExpectedState = 10;
 
-  EXEC tSQLt.Private_NoTransactionHandleTable @Action = 'Save', @FullTableName = '[someschema].[sometable]', @TableAction = 'Unacceptable';
+  EXEC tSQLt.Private_NoTransactionHandleTable @Action = 'Save', @FullTableName = 'Private_NoTransactionHandleTableTests.SomeTable', @TableAction = 'Unacceptable';
 END;
+GO
+/*-----------------------------------------------------------------------------------------------*/
 GO
 CREATE PROCEDURE Private_NoTransactionHandleTableTests.[test augments any internal error with ' tSQLt is in an unknown state: Stopping execution.']
 AS
 BEGIN
-  EXEC tSQLt.SpyProcedure @ProcedureName = 'tSQLt.RemoveObject', @CommandToExecute='RAISERROR(''SOME INTERNAL ERROR.'',16,10)';
+  CREATE TABLE Private_NoTransactionHandleTableTests.SomeTable(i INT);
+  EXEC tSQLt.SpyProcedure @ProcedureName = 'tSQLt.RemoveObject', @CommandToExecute='RAISERROR(''SOME INTERNAL ERROR.'',15,11)';
 
-  EXEC tSQLt.ExpectException @ExpectedMessage = 'SOME INTERNAL ERROR. tSQLt is in an unknown state: Stopping execution.', @ExpectedSeverity = NULL, @ExpectedState = NULL;
-  EXEC tSQLt.Private_NoTransactionHandleTable @Action = 'Save', @FullTableName = 'Some.Table', @TableAction = 'Remove';
+  EXEC tSQLt.ExpectException @ExpectedMessage = 'tSQLt is in an unknown state: Stopping execution. (SOME INTERNAL ERROR. | Procedure: tSQLt.RemoveObject | Line: 1)', @ExpectedSeverity = 15, @ExpectedState = 11;
+  EXEC tSQLt.Private_NoTransactionHandleTable @Action = 'Save', @FullTableName = 'Private_NoTransactionHandleTableTests.SomeTable', @TableAction = 'Remove';
 END;
 GO
+/*-----------------------------------------------------------------------------------------------*/
+GO
+CREATE PROCEDURE Private_NoTransactionHandleTableTests.[test errors if the table does not exist]
+AS
+BEGIN
 
+  EXEC tSQLt.ExpectException @ExpectedMessagePattern = '%Nonexistent.Table does not exist%';
+  EXEC tSQLt.Private_NoTransactionHandleTable @Action = 'someaction', @FullTableName = 'Nonexistent.Table', @TableAction = 'sometableaction';
+END;
+GO
+GO
+/*-----------------------------------------------------------------------------------------------*/
+GO
+CREATE PROCEDURE Private_NoTransactionHandleTableTests.[test copies data from backup into the emptied original table if @Action is Reset and @TableAction is Restore]
+AS
+BEGIN
+  CREATE TABLE Private_NoTransactionHandleTableTests.Table1 (Id INT, col1 NVARCHAR(MAX));
+  INSERT INTO Private_NoTransactionHandleTableTests.Table1 VALUES(1, 'a'),(2, 'bb'),(3, 'cdce');
+  EXEC tSQLt.Private_NoTransactionHandleTable @Action = 'Save', @FullTableName = 'Private_NoTransactionHandleTableTests.Table1', @TableAction = 'Restore';
+
+  TRUNCATE TABLE Private_NoTransactionHandleTableTests.Table1;
+  EXEC tSQLt.Private_NoTransactionHandleTable @Action = 'Reset', @FullTableName = 'Private_NoTransactionHandleTableTests.Table1', @TableAction = 'Restore';
+
+  SELECT * INTO #Actual FROM Private_NoTransactionHandleTableTests.Table1;
+
+  SELECT TOP(0) A.* INTO #Expected FROM #Actual A RIGHT JOIN #Actual X ON 1=0;
+  INSERT INTO #Expected VALUES(1, 'a'),(2, 'bb'),(3, 'cdce');
+
+  EXEC tSQLt.AssertEqualsTable '#Expected','#Actual';
+END;
+GO
+/*-----------------------------------------------------------------------------------------------*/
+GO
+CREATE PROCEDURE Private_NoTransactionHandleTableTests.[test deletes original table data before restoring]
+AS
+BEGIN
+  CREATE TABLE Private_NoTransactionHandleTableTests.Table1 (Id INT, col1 NVARCHAR(MAX));
+  INSERT INTO Private_NoTransactionHandleTableTests.Table1 VALUES(1, 'a'),(2, 'bb'),(3, 'cdce');
+  EXEC tSQLt.Private_NoTransactionHandleTable @Action = 'Save', @FullTableName = 'Private_NoTransactionHandleTableTests.Table1', @TableAction = 'Restore';
+
+  TRUNCATE TABLE Private_NoTransactionHandleTableTests.Table1;
+  INSERT INTO Private_NoTransactionHandleTableTests.Table1 VALUES(4, 'abcdef'),(6, 'dd'),(7, 'khdf');
+  EXEC tSQLt.Private_NoTransactionHandleTable @Action = 'Reset', @FullTableName = 'Private_NoTransactionHandleTableTests.Table1', @TableAction = 'Restore';
+
+  SELECT * INTO #Actual FROM Private_NoTransactionHandleTableTests.Table1;
+
+  SELECT TOP(0) A.* INTO #Expected FROM #Actual A RIGHT JOIN #Actual X ON 1=0;
+  INSERT INTO #Expected VALUES(1, 'a'),(2, 'bb'),(3, 'cdce');
+
+  EXEC tSQLt.AssertEqualsTable '#Expected','#Actual';
+END;
+GO
+/*-----------------------------------------------------------------------------------------------*/
+GO
+CREATE PROCEDURE Private_NoTransactionHandleTableTests.[test can restore table with identity column]
+AS
+BEGIN
+  CREATE TABLE Private_NoTransactionHandleTableTests.Table1 (Id INT IDENTITY (1,1), col1 NVARCHAR(MAX));
+  INSERT INTO Private_NoTransactionHandleTableTests.Table1 VALUES('a'),('bb'),('cdce');
+  EXEC tSQLt.Private_NoTransactionHandleTable @Action = 'Save', @FullTableName = 'Private_NoTransactionHandleTableTests.Table1', @TableAction = 'Restore';
+
+  EXEC tSQLt.Private_NoTransactionHandleTable @Action = 'Reset', @FullTableName = 'Private_NoTransactionHandleTableTests.Table1', @TableAction = 'Restore';
+
+  SELECT * INTO #Actual FROM Private_NoTransactionHandleTableTests.Table1;
+
+  SELECT TOP(0) A.* INTO #Expected FROM #Actual A RIGHT JOIN #Actual X ON 1=0;
+  INSERT INTO #Expected VALUES(1, 'a'),(2, 'bb'),(3, 'cdce');
+
+  EXEC tSQLt.AssertEqualsTable '#Expected','#Actual';
+END;
 /*--
 TODO
-
-- If TableAction is not Restore, throw an error.
-- If FullTableName is not found, throw an error?
-- Save
--- TableAction = Restore
---- Saves an exact copy of the table data into a tSQLt Temp Object table
---- tSQLt Temp Object is marked as IsTempObject = 1
--- All errors are augmented with "tSQLt is in an unknown state: Stopping execution."
 - Reset
--- TableAction = Restore
---- Restores --> truncates original table and uses tSQLt Temp Object table to insert/restore data 
+-- Error conditions
+-- Ignore
+-- Truncate
+-- Remove
 
 --*/
