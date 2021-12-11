c37741d7da21b610ccbd555db2aaace8e6f45dbe
Liz Baron <10554+lizbaron@users.noreply.github.com>
2021-12-11 18:04:26 -0500

    Fixed a failing test by adhering to our existing standards of "Private" as the prefix for objects which do not belong in the facade.

--- Source/tSQLt.Private_Seize.tbl.sql
+++ Source/tSQLt.Private_Seize.tbl.sql
@@ -4,11 +4,11 @@ GO
 ---Build+
 GO
 CREATE TABLE tSQLt.Private_Seize(
-  Kaput BIT CONSTRAINT [PK:Private_Seize] PRIMARY KEY CONSTRAINT [CHK:Private_Seize] CHECK(Kaput=1)
+  Kaput BIT CONSTRAINT [Private_Seize:PK] PRIMARY KEY CONSTRAINT [Private_Seize:CHK] CHECK(Kaput=1)
 );
 GO
 CREATE TABLE tSQLt.Private_Seize_NoTruncate(
-  NoTruncate BIT CONSTRAINT [FK:Private_Seize_NoTruncate(NoTruncate)] FOREIGN KEY REFERENCES tSQLt.Private_Seize(Kaput)
+  NoTruncate BIT CONSTRAINT [Private_Seize_NoTruncate(NoTruncate):FK] FOREIGN KEY REFERENCES tSQLt.Private_Seize(Kaput)
 );
 GO
 CREATE TRIGGER tSQLt.Private_Seize_Stop ON tSQLt.Private_Seize INSTEAD OF DELETE,UPDATE
