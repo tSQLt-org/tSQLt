457e1f74b7447f5112d594bdad34154ab0929123
“mbt1” <mbt1@users.noreply.github.com>
2023-12-02 03:39:36 +0000

    All assemblies are now signed

--- tSQLtCLR/build.ps1
+++ tSQLtCLR/build.ps1
@@ -5,24 +5,22 @@ $randomNumber = Get-Random -Minimum 10000 -Maximum 99999
 $tempDir = Join-Path -Path "/tmp" -ChildPath $("myapp_${PID}_$randomNumber")
 try{
     New-Item -Path $tempDir -ItemType Directory -Force
-    $snkFilePath = Join-Path -Path $tempDir -ChildPath "tSQLtOfficialSigningKey.snk"
-    # Set directory permissions to be accessible only by the current user
     Invoke-Expression "chmod 700 '$tempDir'"
 
-    # Define paths and names
-    $pfxPath = Join-Path -Path $env:TSQLTCERTPATH -ChildPath $("tSQLtOfficialSigningKey.pfx")
+    $snkFilePath = Join-Path -Path $tempDir -ChildPath "tSQLtOfficialSigningKey.snk"
+    $pemFilePath = Join-Path -Path $tempDir -ChildPath "tSQLtOfficialSigningKey.pem"
+    $pfxFilePath = Join-Path -Path $env:TSQLTCERTPATH -ChildPath $("tSQLtOfficialSigningKey.pfx")
     $pfxPassword = $env:TSQLTCERTPASSWORD
 
-    # Extract the private key from the PFX and convert to SNK
-    & openssl pkcs12 -in $pfxPath -nocerts -nodes -passin pass:$pfxPassword | openssl rsa -outform DER -out $snkFilePath
+    & openssl pkcs12 -in "$pfxFilePath" -out "$pemFilePath" -nodes -passin pass:$pfxPassword
 
-    # Check for errors in the last command
-    if ($LASTEXITCODE -ne 0) {
-        Write-Error "Failed to extract and convert the private key"
-        exit $LASTEXITCODE
-    }    
+    $rsa = New-Object System.Security.Cryptography.RSACryptoServiceProvider
+    $pemContent = Get-Content -Path "$pemFilePath" -Raw
+    $rsa.ImportFromPem($pemContent.ToCharArray())
+    $keyPair = $rsa.ExportCspBlob($true)
+    [System.IO.File]::WriteAllBytes($snkFilePath, $keyPair)
 
-    & dotnet build /p:tSQLtOfficialSigningKey=$snkFilePath        
+    & dotnet build /p:tSQLtOfficialSigningKey="$snkFilePath"
 }
 finally {
     try{Remove-Item -Path $tempDir -Recurse -Force}catch{Write-Host "deleting tempdir failed!"}
