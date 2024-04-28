b17578db1f04f65b6dc92eba39c0f572aef6205b
“mbt1” <mbt1@users.noreply.github.com>
2024-04-28 18:02:03 +0000

    moved SQLServerConnection into separate dor-source-able file

--- Build/CommonFunctionsAndMethods.psm1
+++ Build/CommonFunctionsAndMethods.psm1
@@ -1,7 +1,7 @@
 ﻿$__=$__ #quiesce warnings
 $CommonFunctionsAndMethodsDir = $PSScriptRoot
 Write-Host "Loading CommonFunctionsAndMethods.psm1 from: $PSCommandPath"
-
+. (Join-Path $CommonFunctionsAndMethodsDir 'SQLServerConnection.ps1');
 
 $MergeHashTables = {param([HashTable]$base,[HashTable]$new);$new.GetEnumerator()|%{$base.remove($_.Key);$base += @{$_.Key=$_.Value}};$base;};
 $AddTagsToResourceGroup = 
@@ -14,95 +14,6 @@ $GetUTCTimeStamp = {param();(Get-Date).ToUniversalTime().ToString('[yyyy-MM-ddTH
 $SQLPrintCurrentTime = "EXEC('DECLARE @C VARCHAR(MAX)=CONVERT(VARCHAR(MAX),SYSUTCDATETIME(),127);RAISERROR(@C,0,1)WITH NOWAIT;');";
 
 Function Log-Output{[cmdletbinding()]Param([parameter(ValueFromPipeline)]$I);Process{Write-Host ([string]::Concat($GetUTCTimeStamp.Invoke()[0],[string]::Concat(" $I")));};};
-
-if (-not ([System.Management.Automation.PSTypeName]'SqlServerConnection').Type) {
-
-  class SqlServerConnection {
-    hidden [string]$ServerName
-    hidden [string]$UserName
-    hidden [System.Security.SecureString]$Password
-    hidden [bool]$TrustedConnection
-    hidden [string]$ApplicationName
-    hidden [string]$BaseConnectionString = "Connect Timeout=60;TrustServerCertificate=true;"
-
-    SqlServerConnection([string]$ServerName, [string]$UserName, [System.Security.SecureString]$Password,[string]$ApplicationName) {
-      $this.ServerName = $ServerName.Trim()
-      $this.UserName = $UserName.Trim()
-      $this.Password = $Password
-      $this.TrustedConnection = false
-      $this.ApplicationName = $ApplicationName.Trim()
-    }
-    SqlServerConnection([string]$ServerName,[string]$ApplicationName) {
-      $this.ServerName = $ServerName.Trim()
-      $this.UserName = $null
-      $this.Password = $null
-      $this.TrustedConnection = true
-      $this.ApplicationName = $ApplicationName.Trim()
-    }
-
-    [string] ToString() {
-      
-
-      return @{
-        ServerName= $($this.ServerName)
-        UserName= $($this.UserName)
-        Password= (ConvertFrom-SecureString $this.Password -AsPlainText)
-        TrustedConnection= $($this.TrustedConnection)
-        ApplicationName= $($this.ApplicationName)
-        BaseConnectionString= $($this.BaseConnectionString)
-      }| ConvertTo-Json -Compress
-    } 
-
-    static [string] ToStringStatic([SqlServerConnection]$instance) {
-        if ($null -eq $instance) {
-            return "$null"
-        }
-        else {
-            return $instance.ToString()
-        }
-    }
-
-    [string] GetServerName() {
-        return $this.ServerName
-    }
-
-    [string] GetUserName() {
-        return $this.UserName
-    }
-
-    [System.Security.SecureString] GetPassword() {
-        return $this.Password
-    }
-
-    [bool] GetTrustedConnection() {
-        return $this.TrustedConnection
-    }
-
-    hidden [string] EscapeAndQuoteConnectionStringValue([string]$value) {
-      return "`"$( $value.Replace('"', '""') )`""
-    }
-
-    [string] GetConnectionString([string]$DatabaseName = '', [string]$ApplicationNameSuffix = '') {
-        $connectionString = "Server=$($this.EscapeAndQuoteConnectionStringValue($this.ServerName));"
-
-        if ($this.TrustedConnection) {
-            $connectionString += "Integrated Security=SSPI;"
-        } else {
-          $connectionString += "User Id=$($this.EscapeAndQuoteConnectionStringValue($this.UserName));"
-          $connectionString += "Password=$($this.EscapeAndQuoteConnectionStringValue((ConvertFrom-SecureString $this.Password -AsPlainText)));"
-        }
-        if(![string]::IsNullOrWhiteSpace($DatabaseName)){
-          $connectionString += "Initial Catalog=$($this.EscapeAndQuoteConnectionStringValue($DatabaseName));"
-        }
-        $PApplicationName = $this.ApplicationName;
-        if(![string]::IsNullOrWhiteSpace($ApplicationNameSuffix)){
-          $PApplicationName+=".$ApplicationNameSuffix"
-        }
-        $connectionString += "Application Name=$($this.EscapeAndQuoteConnectionStringValue($PApplicationName));"
-        return $this.BaseConnectionString+$connectionString
-    }
-  }
-}
 
 Function Exec-SqlFile
 {
